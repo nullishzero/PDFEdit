9c745c8fb392753b5834c976ce5a133b144365c5
Martin Petříček <tux@centrum.cz>
2005-12-09 23:04:36 +0000

    pridany makra min() a max()

--- src/util.h
+++ src/util.h
@@ -3,6 +3,13 @@
 #include <qstring.h>
 #include <qstringlist.h>
 
+/** macro returning greater of two values */
+#define max(x,y) ((x)>=(y)?(x):(y))
+
+/** macro returning lesser of two values */
+#define min(x,y) ((x)<=(y)?(x):(y))
+
+
 void fatalError(const QString message);
 QStringList explode(char separator,const QString line);
 
