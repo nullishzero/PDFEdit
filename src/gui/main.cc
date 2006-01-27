dd13e038c80c81c33aa218e7f37169cfbf97e8e9
Martin Petříček <tux@centrum.cz>
2006-01-27 21:17:37 +0000

    presunuto ze src do src/gui + male upravy (ostraneni kernel-specific casti)

---
+++ src/gui/main.cc
@@ -0,0 +1,25 @@
+/** @file
+ Main function
+*/
+#include "pdfeditwindow.h"
+#include "settings.h"
+#include <stdlib.h>
+
+Settings *global;
+
+/** delete settings object (and save settings)
+ This function is called at application exit
+ */
+void saveSettings(void) {
+ delete global;//this causes settings to be saved to disk
+}
+
+/** main - load settings and launches a main window */
+int main(int argc, char *argv[]){
+ QApplication app(argc, argv);
+ global=new Settings();
+ atexit(saveSettings);
+ createNewEditorWindow();
+ QObject::connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
+ return app.exec();
+}
---
+++ src/gui/pdfedit.pro
@@ -0,0 +1,12 @@
+#Project file
+TEMPLATE = app
+
+#tohle nefunguje. Resim jinak
+#load( qsa )
+
+unix:LIBS       += -lqsa
+
+HEADERS += pdfeditwindow.h settings.h util.h toolbutton.h toolbar.h property.h stringproperty.h propertyeditor.h intproperty.h
+SOURCES += pdfeditwindow.cc settings.cc main.cc util.cc toolbutton.cc toolbar.cc property.cc stringproperty.cc propertyeditor.cc intproperty.cc
+
+
