ca19674bb4a5dd4547af19f63233e3c16efe49c3
Jozef Mišutka <misutkajunior@hotmail.com>
2006-01-27 20:53:05 +0000

    malicke doladovanie..jm

---
+++ src/kernel/main.cc
@@ -0,0 +1,28 @@
+/**
+ * main_kernel.cc
+ *
+ * Kernel does not need QT 
+ */
+
+#include <stdlib.h>
+#include "cobject.h"
+
+
+using namespace std;
+
+
+/**
+ *  test main - load settings and launches a main window 
+ */
+int 
+main ()
+{
+
+
+
+
+
+
+
+return 0;
+}
