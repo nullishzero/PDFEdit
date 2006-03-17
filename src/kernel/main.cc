1d67b77703f2aa9455ee811b51e08c203faffeae
Jozef Mišutka <misutkajunior@hotmail.com>
2006-03-17 18:01:16 +0000

    -- added tests

--- src/kernel/main.cc
+++ src/kernel/main.cc
@@ -1,7 +1,6 @@
 /*
  * main.cc
  *
- * Kernel does not need QT 
  */
 
 #include "static.h"
@@ -22,21 +21,23 @@
 
 //========= NASTY =============
 #ifdef NO_KERNEL_COUT_OUTPUT
-	#define KERNEL_OUTPUT 	ofstream redirect_file("/dev/null"); streambuf * strm_buffer = cout.rdbuf();cout.rdbuf(redirect_file.rdbuf());
-	#define KERNEL_OUTPUT_BACK	 cout.rdbuf(strm_buffer);
+	#define KERNEL_OUTPUT 		ofstream redirect_file("/dev/null"); streambuf * strm_buffer = NULL;\
+								if (1 == argc) {strm_buffer = cout.rdbuf();cout.rdbuf(redirect_file.rdbuf());}
+	#define KERNEL_OUTPUT_BACK	if (1 == argc) {cout.rdbuf(strm_buffer);}
 	#define OUTPUT	cerr
 #else
-	#define KERNEL_OUTPUT
-	#define KERNEL_OUTPUT_BACK
+	#define KERNEL_OUTPUT 		ofstream redirect_file("/dev/null"); streambuf * strm_buffer = NULL;\
+								if (1 != argc) {strm_buffer = cout.rdbuf();cout.rdbuf(redirect_file.rdbuf());}
+	#define KERNEL_OUTPUT_BACK	if (1 != argc) {cout.rdbuf(strm_buffer);}
 	#define	OUTPUT 	cout
 #endif
+
+#define MEM_CHECK	{BasicMemChecker check;OUTPUT  << "OBJECTS UNALLOCATED: " << check.getCount () << endl;}
 
 #define TEST(a) 	OUTPUT << endl << endl << "//=================== " << (a) << endl << endl;
 #define START_TEST 	OUTPUT << endl << "Started testing..." << endl; KERNEL_OUTPUT;
 #define END_TEST	OUTPUT << endl << "Ended testing..." << endl; KERNEL_OUTPUT_BACK;
-#define OK_TEST		OUTPUT << "TEST PASSED..." << endl;
-
-#define MEM_CHECK	BasicMemChecker check;OUTPUT  << "OBJECTS UNALLOCATED: " << check.getCount () << endl;
+#define OK_TEST		OUTPUT << "TEST PASSED..." << endl; MEM_CHECK;
 //==========================
 
 
@@ -75,12 +76,20 @@ typedef struct
 
 typedef struct
 {
-	example_item b;
-	example_item i;
-	example_item r;
-	example_item s;
-	example_item n;
-	example_item ref;
+	// simple
+	example_item 	b;
+	example_item 	i;
+	example_item 	r;
+	example_item 	s;
+	example_item	n;
+	example_item 	ref;
+
+	// complex
+	string			arTest1;
+	string			arTest2;
+	string			dcTest1;
+	string			dcTest2;
+
 } example;
 
 
@@ -189,7 +198,7 @@ namespace {
 	}
 
 	bool
-	ip_validate (Object* o, const string& expected)
+	ip_validate (Object* o, const string& expected, bool out = true)
 	{
 		string str;
 		assert (NULL != o);
@@ -197,7 +206,10 @@ namespace {
 
 		if (str == expected)
 		{
-			OUTPUT << "*** Validation OK! " << expected << endl;
+			if (out)
+				OUTPUT << "*** Validation OK! " << expected << endl;
+			else
+				OUTPUT << "*** Validation OK! " << "<not shown, output probably too long>"<< endl;
 			return true;
 		}
 		else
@@ -209,19 +221,97 @@ namespace {
 	}
 
 	bool
-	ip_validate (const IProperty& ip, const string& expected)
+	ip_validate (const IProperty& ip, const string& expected, bool out = true)
 	{
 		string str;
 		ip.getStringRepresentation (str);
 		
 		if (str == expected)
 		{
-			OUTPUT << "*** Validation OK! " << expected << endl;
+			if (out)
+				OUTPUT << "*** Validation OK! " << expected << endl;
+			else
+				OUTPUT << "*** Validation OK! " << "<not shown, output probably too long>"<< endl;
+					
 			return true;
 		}
 		else
 		{
 			OUTPUT << "DOES NOT MATCH: " << str << " with " << expected << endl;
+			throw;
+			return false;
+		}
+	}
+	
+	bool
+	ip_validate (const IProperty& ip1, const IProperty& ip2)
+	{
+		string str1, str2;
+		ip1.getStringRepresentation (str1);
+		ip2.getStringRepresentation (str2);
+		
+		if (str1 == str2)
+		{
+			OUTPUT << "*** Validation OK! " << str1 << endl;
+			return true;
+		}
+		else
+		{
+			OUTPUT << "DOES NOT MATCH: " << str1 << " with " << str2 << endl;
+			throw;
+			return false;
+		}
+	}
+
+	bool
+	ip_validate (PropertyType tp1, PropertyType tp2)
+	{
+		if (tp1 == tp2)
+		{
+			OUTPUT << "*** validation ok! " << tp1 << endl;
+			return true;
+		}
+		else
+		{
+			OUTPUT << "does not match: " << tp1 << " with " << tp2 << endl;
+			throw;
+			return false;
+		}
+	}
+
+	bool
+	ip_validate (size_t n, size_t m)
+	{
+		if (n == m)
+		{
+			OUTPUT << "*** validation ok! size:" << n << endl;
+			return true;
+		}
+		else
+		{
+			OUTPUT << "does not match: " << n << " with " << m << endl;
+			throw;
+			return false;
+		}
+	}
+
+	bool
+	ip_validate (vector<string>& n, vector<string>& m)
+	{
+		if (equal (n.begin(), n.end(), m.begin()))
+		{
+			OUTPUT << "*** validation ok!" <<  endl;
+			return true;
+		}
+		else
+		{
+			OUTPUT << "does not match: ";
+			for (vector<string>::iterator it = n.begin (); it != n.end(); ++it)
+					OUTPUT << *it << "|";
+			OUTPUT << "... with ...";
+			for (vector<string>::iterator it = m.begin (); it != m.end(); ++it)
+					OUTPUT << *it << "|";
+			OUTPUT << endl;
 			throw;
 			return false;
 		}
@@ -245,8 +335,12 @@ void
 s_clone ()
 {
 		CNull null;
-		null.clone (); // object UNALLOCATED
-		OUTPUT << "null.getType(): " << null.getType () << endl;
+		boost::shared_ptr<IProperty> ptr = null.clone ();
+
+		if (ptr)
+			OUTPUT << "null.getType(): " << null.getType () << endl;
+		else 
+			throw;
 }
 
 //====================================================
@@ -257,9 +351,9 @@ s_clone ()
 void 
 s_ctors (const example& e)
 {
-		static Object obj;
-		static CPdf pdf;
-		static IndiRef ref;
+		Object obj;
+		CPdf pdf;
+		IndiRef ref;
 		
 		// bool
 		obj.initBool (e.b.xb);
@@ -368,6 +462,40 @@ s_setString (const example& e)
 		// null.setStringRepresentation ("");
 }
 
+//====================================================
+
+namespace
+{
+		template<typename T,PropertyType Tp>
+		void _s_getTp ()
+		{
+				T cl;
+				ip_validate (Tp,cl.getType ());
+		}
+
+}
+
+//
+//
+//
+void 
+s_getTp ()
+{
+		// bool
+		_s_getTp<CBool,pBool> ();
+		// int
+		_s_getTp<CInt,pInt> ();
+		// Real
+		_s_getTp<CReal,pReal> ();
+		// String
+		_s_getTp<CString,pString> ();
+		// Name
+		_s_getTp<CName,pName> ();
+		// Ref
+		_s_getTp<CRef,pRef> ();
+		// Null
+		_s_getTp<CNull,pNull> ();
+}
 
 
 //====================================================
@@ -516,8 +644,8 @@ c_clone ()
 		CString item1 ("fero");
 		dict.addProperty ("jano", item1);
 		
-		boost::shared_ptr<IProperty> clone_ = dict.clone (); // 2x object UNALLOCATED
-		OUTPUT << dict;
+		boost::shared_ptr<IProperty> clone_ = dict.clone (); // 2x objects allocated
+		ip_validate (dict, *clone_);
 }
 
 
@@ -531,16 +659,52 @@ c_smrt ()
 		CString val2 ("val2");
 		dict.addProperty ("item1", val1);
 		dict.addProperty ("item2", val2);
-		CString* str = new CString ("val3");	// UNALLOCATED if not freed manually
-		dict.addProperty ("item3", *str);
+
+// BAD HABBIT
+		CString* ptrStr1 = new CString ("val3");	// UNALLOCATED if not freed manually
+		dict.addProperty ("item3", *ptrStr1);
+		delete ptrStr1;
+// GOOD HABBIT
+		boost::shared_ptr<IProperty> ptrStr2 (new CString ("val4"));
+		dict.addProperty ("item4", *ptrStr2);
+// EVEN BETTER HABBIT
+		CString str3 ("val5");
+		dict.addProperty ("item5", str3);
+// BEST HABBIT -- not functional (yet)
+		//dict.addProperty ("item5", CString ("val6"));
 
 		OUTPUT << dict;
 		
-		boost::shared_ptr<IProperty> ipp = dict.getPropertyValue ("item3");
-
-		delete str;
+		boost::shared_ptr<IProperty> ipp1 = dict.getPropertyValue ("item3");
+		boost::shared_ptr<IProperty> ipp2 = dict.getPropertyValue ("item4");
+		boost::shared_ptr<IProperty> ipp3 = dict.getPropertyValue ("item5");
 }
 
+//=====================================================================================
+
+namespace
+{
+		template<typename U, PropertyType Tp>
+		void
+		_c_getTp ()
+		{
+			U obj;
+			ip_validate (Tp, obj.getType ());
+		}
+}
+
+void
+c_getTp ()
+{
+		// Array
+		_c_getTp<CArray,pArray> ();
+		// Dict
+		_c_getTp<CDict,pDict> ();
+		// Stream
+		_c_getTp<CStream,pStream> ();
+
+
+}
 
 //=====================================================================================
 
@@ -554,6 +718,341 @@ mdctrl ()
 
 //=====================================================================================
 
+
+
+namespace
+{
+		void
+		makeArTest1 (CArray& arTest1)
+		{
+			//
+			// Make an array, simple values
+			//
+			CString it1 ("item1");
+			CString it2 ("item2");
+			CString it3 ("item3");
+			arTest1.addProperty (it1);
+			arTest1.addProperty (it2);
+			arTest1.addProperty (it3);
+			CInt i1 (1);
+			CInt i2 (2);
+			CInt i3 (3);
+			CInt i4 (4);
+			arTest1.addProperty (i1);
+			arTest1.addProperty (i2);
+			arTest1.addProperty (i3);
+			arTest1.addProperty (i4);
+			IndiRef ref = {3,4};
+			CRef rf (ref);
+			arTest1.addProperty (rf);
+			CBool bl (false);
+			arTest1.addProperty (bl);
+
+		}
+	
+		void
+		makeDcTest1 (CDict& dcTest1)
+		{
+			//
+			// Make a dictionary, simple values
+			//
+			CString it1 ("item1");
+			CString it2 ("item2");
+			CString it3 ("item3");
+			dcTest1.addProperty ("item1", it1);
+			dcTest1.addProperty ("item2", it2);
+			dcTest1.addProperty ("item3", it3);
+			CInt i1 (1);
+			CInt i2 (2);
+			CInt i3 (3);
+			CInt i4 (4);
+			dcTest1.addProperty ("item4", i1);
+			dcTest1.addProperty ("item5", i2);
+			dcTest1.addProperty ("item6", i3);
+			dcTest1.addProperty ("item7", i4);
+			IndiRef ref = {3,4};
+			CRef rf (ref);
+			dcTest1.addProperty ("item8", rf);
+			CBool bl (false);
+			dcTest1.addProperty ("item9", bl);
+
+		}
+
+		void
+		makeArTest2 (CArray& arTest2, CArray& arTest1, CDict& dcTest1)
+		{
+			arTest2.addProperty (arTest1);
+			arTest2.addProperty (arTest1);
+			arTest2.addProperty (arTest1);
+			arTest2.addProperty (dcTest1);
+			arTest2.addProperty (dcTest1);
+			arTest2.addProperty (dcTest1);
+		}
+
+		void
+		makeDcTest2 (CDict& dcTest2, CArray& arTest1, CDict& dcTest1)
+		{
+			dcTest2.addProperty ("1", arTest1);
+			dcTest2.addProperty ("2", arTest1);
+			dcTest2.addProperty ("3", arTest1);
+			dcTest2.addProperty ("4", dcTest1);
+			dcTest2.addProperty ("5", dcTest1);
+			dcTest2.addProperty ("6", dcTest1);
+		}
+		
+}
+
+void
+c_getString (const example& e)
+{
+		
+	CArray arTest1;
+	CArray arTest2;
+	CDict dcTest1;
+	CDict dcTest2;
+
+	makeArTest1 (arTest1);
+	ip_validate (arTest1, e.arTest1);
+
+	makeDcTest1 (dcTest1);
+	ip_validate (dcTest1, e.dcTest1, false);
+
+	makeArTest2 (arTest2, arTest1, dcTest1);
+	ip_validate (arTest2, e.arTest2, false); 
+
+	makeDcTest2 (dcTest2, arTest1, dcTest1);
+	ip_validate (dcTest2, e.dcTest2, false); 
+}
+
+
+//=====================================================================================
+
+void
+c_getCnt ()
+{
+		
+	CArray arTest1;
+	CArray arTest2;
+	CDict dcTest1;
+	CDict dcTest2;
+
+	makeArTest1 (arTest1);
+	ip_validate (arTest1.getPropertyCount (), 9);
+
+	makeDcTest1 (dcTest1);
+	ip_validate (dcTest1.getPropertyCount (), 9);
+
+	
+	makeArTest2 (arTest2, arTest1, dcTest1);
+	ip_validate (arTest2.getPropertyCount (), 6); 
+
+	makeDcTest2 (dcTest2, arTest1, dcTest1);
+	ip_validate (dcTest2.getPropertyCount (), 6); 
+}
+
+//=====================================================================================
+
+void
+c_getNames ()
+{
+		
+	CArray arTest1;
+	CDict dcTest1;
+	CDict dcTest2;
+
+	// create Array 1
+	makeArTest1 (arTest1);
+
+	//
+	// expected item1, ... ,item9
+	//
+	vector<string> expected;
+	for (int i = 1; i <= 9; i++)
+	{
+		ostringstream oss;
+		oss << i;
+		expected.push_back (string ("item" + oss.str()));
+	}
+	
+	// create dictionary 1
+	makeDcTest1 (dcTest1);
+	vector<string> names;
+	dcTest1.getAllPropertyNames (names);
+	ip_validate (names, expected);
+
+	//
+	// reset string
+	//
+	expected.clear ();
+	names.clear ();
+	
+	//
+	// other 1, ... ,9
+	//
+	for (int i = 1; i <= 9; i++)
+	{
+		ostringstream oss;
+		oss << i;
+		expected.push_back (string (oss.str()));
+	}
+
+	makeDcTest2 (dcTest2, arTest1, dcTest1);
+	dcTest2.getAllPropertyNames (names);
+	ip_validate (names, expected); 
+}
+
+		
+//=====================================================================================
+		
+void
+c_getType ()
+{
+		
+	CArray arTest1;
+	CDict dcTest1;
+
+	// create Array 1
+	makeArTest1 (arTest1);
+	for (size_t i = 0; i < arTest1.getPropertyCount (); i++)
+	{
+		boost::shared_ptr<IProperty> ip = arTest1.getPropertyValue (i);
+		ip_validate (arTest1.getPropertyType (i), ip->getType ());
+	}
+	
+	
+	// create dictionary 1
+	makeDcTest1 (dcTest1);
+	vector<string> names;
+	dcTest1.getAllPropertyNames (names);
+	
+	for (vector<string>::iterator it = names.begin(); it != names.end(); ++it)
+	{
+		boost::shared_ptr<IProperty> ip = dcTest1.getPropertyValue (*it);
+		ip_validate (dcTest1.getPropertyType (*it), ip->getType ());
+	}
+
+}
+
+//=====================================================================================
+
+void
+c_xpdf (const example& e)
+{
+
+	CArray arTest1;
+	CArray arTest2;
+	CDict dcTest1;
+	CDict dcTest2;
+	string tmp;
+
+	makeArTest1 (arTest1);
+	Object* obj = arTest1._makeXpdfObject ();
+	arTest1.getStringRepresentation (tmp);
+	ip_validate (obj, e.arTest1);
+	ip_validate (obj, tmp);
+	utils::freeXpdfObject (obj);
+
+	makeDcTest1 (dcTest1);
+	obj = dcTest1._makeXpdfObject ();
+	dcTest1.getStringRepresentation (tmp);
+	ip_validate (obj, e.dcTest1, false);
+	ip_validate (obj, tmp);
+	utils::freeXpdfObject (obj);
+
+	makeArTest2 (arTest2, arTest1, dcTest1);
+	obj = arTest2._makeXpdfObject ();
+	arTest2.getStringRepresentation (tmp);
+	ip_validate (obj, e.arTest2, false);
+	ip_validate (obj, tmp);
+	utils::freeXpdfObject (obj);
+
+
+	makeDcTest2 (dcTest2, arTest1, dcTest1);
+	obj = dcTest2._makeXpdfObject ();
+	dcTest2.getStringRepresentation (tmp);
+	ip_validate (obj, e.dcTest2, false);
+	ip_validate (obj, tmp);
+	utils::freeXpdfObject (obj);
+}
+
+//=====================================================================================
+
+void
+c_del ()
+{
+
+	CArray arTest1;
+	CArray arTest2;
+	CDict dcTest1;
+	CDict dcTest2;
+
+	//
+	//
+	//
+	makeArTest1 (arTest1);
+	while (0 != arTest1.getPropertyCount())
+		arTest1.delProperty (0);
+	ip_validate (arTest1,"[ ]");
+
+	//
+	//
+	//
+	makeDcTest1 (dcTest1);
+	list<string> names;
+	dcTest1.getAllPropertyNames (names);
+	for (list<string>::iterator it = names.begin (); it != names.end (); ++it)
+		dcTest1.delProperty (*it);
+	ip_validate (dcTest1,"<<\n>>");
+
+	//
+	//
+	//
+	makeArTest2 (arTest2, arTest1, dcTest1);
+	while (0 != arTest2.getPropertyCount())
+		arTest2.delProperty (0);
+	ip_validate (arTest2,"[ ]");
+
+	//
+	//
+	//
+	makeDcTest2 (dcTest2, arTest1, dcTest1);
+	names.clear ();
+	dcTest2.getAllPropertyNames (names);
+	for (list<string>::iterator it = names.begin (); it != names.end (); ++it)
+		dcTest2.delProperty (*it);
+	ip_validate (dcTest2,"<<\n>>");
+}
+
+
+//=====================================================================================
+
+void
+c_set ()
+{
+
+	CArray a;
+	CDict d;
+
+	CInt i1 (1);	CInt i2 (2);	CInt i3 (3);
+	CString s1 ("s1");	CString s2 ("s2");	CString s3 ("s3");
+
+	a.addProperty (i1);			a.addProperty (i2);			a.addProperty (i3);
+	ip_validate (a,"[ 1 2 3 ]");
+	
+	a.setPropertyValue (0,s1);	a.setPropertyValue (1,s2);	a.setPropertyValue (2,s3);
+	ip_validate (a,"[ (s1) (s2) (s3) ]");
+
+	d.addProperty ("1", i1);		d.addProperty ("2", i2);		d.addProperty ("3", i3);
+	ip_validate (d,"<<\n/1 1\n/2 2\n/3 3\n>>", false);
+	
+	d.setPropertyValue ("1",s1);	d.setPropertyValue ("2",s2);	d.setPropertyValue ("3",s3);
+	ip_validate (d,"<<\n/1 (s1)\n/2 (s2)\n/3 (s3)\n>>", false);
+}
+
+
+				
+//=====================================================================================
+
 } // namespace
 
 
@@ -562,13 +1061,16 @@ mdctrl ()
 /**
  *  test main - load settings and launches a main window 
  */
-int main ()
+int 
+main (int argc, char* [])
 {
 
 		static example e;
 	
 		//
 		// TEST EXAMPLE
+		//
+		// Simple
 		//
 		e.b.xb 			= gFalse;
 		e.b.b 			= false;
@@ -599,10 +1101,24 @@ int main ()
 		e.ref.value 	= "32 10";
 		e.ref.expected 	= "32 10 R";
 		//
+		// Complex
 		//
-		//
+		e.arTest1 = "[ (item1) (item2) (item3) 1 2 3 4 3 4 R false ]";
+		e.dcTest1 = "<<\n/item1 (item1)\n/item2 (item2)\n/item3 (item3)\n/item4 1\n/item5 2\n/item6 "
+				    "3\n/item7 4\n/item8 3 4 R\n/item9 false\n>>";
+		const string ar2 ("[ " 	+ e.arTest1 + " " + e.arTest1 + " " 
+						 	  	+ e.arTest1 + " " + e.dcTest1 + " " + e.dcTest1 + " " + e.dcTest1 + " ]");
+		const string dc2 ("<<\n/1 "	+ e.arTest1 + "\n/2 " + e.arTest1 + "\n/3 " + e.arTest1 + "\n/4 " 
+									+ e.dcTest1 + "\n/5 " + e.dcTest1 + "\n/6 " + e.dcTest1 + "\n>>");
+		e.arTest2 = ar2;
+		e.dcTest2 = dc2;
 
+	
 		START_TEST;
+		
+		TEST(" test 1.0 -- getType_");
+		s_getTp ();
+		OK_TEST;
 
 		TEST(" test 1.1 -- clone");
 		s_clone ();
@@ -635,6 +1151,12 @@ int main ()
 		TEST(" test 1.8 -- _");
 		s_rel ();
 		OK_TEST;
+	
+		//======================= CObjectComplex
+		
+		TEST(" test 2.1.0 - getType")
+		c_getTp ();
+		OK_TEST;
 
 		TEST(" test 2.1 - clone")
 		c_clone ();
@@ -648,6 +1170,33 @@ int main ()
 		mdctrl ();
 		OK_TEST;
 	
+		TEST(" test 2.4 - getStringRepre")
+		c_getString (e);
+		OK_TEST;
+
+		TEST(" test 2.5 - getPropCount")
+		c_getCnt ();
+		OK_TEST;
+
+		TEST(" test 2.6 - getAllPropNames")
+		c_getNames ();
+		OK_TEST;
+
+		TEST(" test 2.7 - getType")
+		c_getType ();
+		OK_TEST;
+
+		TEST(" test 2.8 - makeXpdf")
+		c_xpdf (e);
+		OK_TEST;
+
+		TEST(" test 2.9 - delProp")
+		c_del ();
+		OK_TEST;
+
+		TEST(" test 2.9 - setProp")
+		c_set ();
+		OK_TEST;
 
 		END_TEST;
 		MEM_CHECK;
