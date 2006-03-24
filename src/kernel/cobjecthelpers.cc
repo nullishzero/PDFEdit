43821213eccaae379dd2d55998d77b6a46c2cb02
Michal Hocko <mstsxfx@gmail.com>
2006-03-24 17:37:57 +0000

    new file for helper methods for cobjects in this time only for simple methods from dictionary

---
+++ src/kernel/cobjecthelpers.cc
@@ -0,0 +1,113 @@
+// vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
+
+/*
+ * $RCSfile$
+ *
+ * $Log$
+ * Revision 1.1  2006/03/24 17:37:57  hockm0bm
+ * new file for helper methods for cobjects
+ * in this time only for simple methods from dictionary
+ *
+ *
+ */
+
+
+#include <string>
+#include "cobjecthelpers.h"
+
+namespace pdfobjects
+{
+
+namespace utils
+{
+
+
+int getIntFromDict(std::string name, boost::shared_ptr<CDict> dict)
+{
+	using namespace boost;
+
+	shared_ptr<IProperty> prop_ptr=dict->getPropertyValue(name);	
+	if(prop_ptr->getType() != pInt)
+	{
+		// malformed dictionary
+		throw ElementBadTypeException(name/*, prop_ptr->getType()*/);
+	}
+
+	shared_ptr<CInt> int_ptr=IProperty::getSmartCObjectPtr<CInt>(prop_ptr);
+	int value;
+	int_ptr->getPropertyValue(value);
+
+	return value;
+}
+
+IndiRef getRefFromDict(std::string name, boost::shared_ptr<CDict> dict)
+{
+	using namespace boost;
+
+	shared_ptr<IProperty> prop_ptr=dict->getPropertyValue(name);	
+	if(prop_ptr->getType() != pRef)
+	{
+		// malformed dictionary
+		throw ElementBadTypeException(name/*, prop_ptr->getType()*/);
+	}
+
+	shared_ptr<CRef> int_ptr=IProperty::getSmartCObjectPtr<CRef>(prop_ptr);
+	IndiRef value;
+	int_ptr->getPropertyValue(value);
+
+	return value;
+}
+
+std::string getStringFromDict(std::string name, boost::shared_ptr<CDict> dict)
+{
+	using namespace boost;
+
+	shared_ptr<IProperty> prop_ptr=dict->getPropertyValue(name);	
+	if(prop_ptr->getType() != pString)
+	{
+		// malformed dictionary
+		throw ElementBadTypeException(name);
+	}
+
+	shared_ptr<CString> str_ptr=IProperty::getSmartCObjectPtr<CString>(prop_ptr);
+	std::string value;
+	str_ptr->getPropertyValue(value);
+
+	return value;
+}
+	
+std::string getNameFromDict(std::string name, boost::shared_ptr<CDict> dict)
+{
+	using namespace boost;
+
+	shared_ptr<IProperty> prop_ptr=dict->getPropertyValue(name);	
+	if(prop_ptr->getType() != pName)
+	{
+		// malformed dictionary
+		throw ElementBadTypeException(name);
+	}
+
+	shared_ptr<CName> name_ptr=IProperty::getSmartCObjectPtr<CName>(prop_ptr);
+	std::string value;
+	name_ptr->getPropertyValue(value);
+
+	return value;
+}
+
+std::string getDictType(boost::shared_ptr<CDict> dict)
+{
+	try
+	{
+		return getNameFromDict("/Type", dict);
+	// FIXME change to proper exception
+	}catch(std::exception & e)
+	{
+		// not found so returns empty string
+	}
+
+	return std::string();
+}
+
+} // end of utils namespace
+
+} // end of pdfobjects namespace
---
+++ src/kernel/cobjecthelpers.h
@@ -0,0 +1,95 @@
+// vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
+
+#ifndef _COBJECTHELPERS_H_
+#define _COBJECTHELPERS_H_
+
+/*
+ * $RCSfile$
+ *
+ * $Log$
+ * Revision 1.1  2006/03/24 17:37:57  hockm0bm
+ * new file for helper methods for cobjects
+ * in this time only for simple methods from dictionary
+ *
+ *
+ */
+
+#include"cobjectI.h"
+
+namespace pdfobjects
+{
+namespace utils
+{
+
+/** Gets type of the dictionary.
+ * @param dict Dictionary wrapped in smart pointer.
+ *
+ * Tries to get /Type field from dictionary and returns its string value. If not
+ * present, returns an empty string.
+ *
+ * @return string name of the dictionary type or empty string if not able to
+ * find out.
+ */
+std::string getDictType(boost::shared_ptr<CDict> dict);
+        
+/** Helper method for getting int property value from dictionary.
+ * @param name Name of the property in the dictionary.
+ * @param dict Dictionary where to search.
+ *
+ * Gets property according name. Checks property type and if it is realy
+ * pInt, gets its int value which is returned.
+ *
+ * @throw ElementNotFoundException if property is not found.
+ * @throw ElementBadTypeException if property is found but doesn't contain 
+ * integer value.
+ * @return int value of the property.
+ */
+int getIntFromDict(std::string name, boost::shared_ptr<CDict> dict);
+
+/** Helper method for getting reference property value from dictionary.
+ * @param name Name of the property in the dictionary.
+ * @param dict Dictionary where to search.
+ *
+ * Gets property according name. Checks property type and if it is realy
+ * pRef, gets its IndiRef value which is returned.
+ *
+ * @throw ElementNotFoundException if property is not found.
+ * @throw ElementBadTypeException if property is found but doesn't contain 
+ * reference value.
+ * @return IndiRef value of the property.
+ */
+IndiRef getRefFromDict(std::string name, boost::shared_ptr<CDict> dict);
+
+/** Helper method for getting string property value from dictionary.
+ * @param name Name of the property in the dictionary.
+ * @param dict Dictionary where to search.
+ *
+ * Gets property according name. Checks property type and if it is realy
+ * pString, gets its string value which is returned.
+ *
+ * @throw ElementNotFoundException if property is not found.
+ * @throw ElementBadTypeException if property is found but doesn't contain 
+ * string value.
+ * @return std::string value of the property.
+ */
+std::string getStringFromDict(std::string name, boost::shared_ptr<CDict> dict);
+
+/** Helper method for getting name property value from dictionary.
+ * @param name Name of the property in the dictionary.
+ * @param dict Dictionary where to search.
+ *
+ * Gets property according name. Checks property type and if it is realy
+ * pName, gets its string value which is returned.
+ *
+ * @throw ElementNotFoundException if property is not found.
+ * @throw ElementBadTypeException if property is found but doesn't contain 
+ * string value.
+ * @return std::string value of the property.
+ */
+std::string getNameFromDict(std::string name, boost::shared_ptr<CDict> dict);
+
+}// end of utils namespace
+
+}// end of pdfobjects namespace
+#endif
+
