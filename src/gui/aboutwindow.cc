d5eb066d163a7b06a3e8594276c650eafa7e17f5
Martin Petříček <tux@centrum.cz>
2006-04-01 02:20:30 +0000

    Zjistil jsem ze QLabel umi zobrazovat i richtext (neboli HTML) takze jsem aboutbox trochu zcivilizoval.

--- src/gui/aboutwindow.cc
+++ src/gui/aboutwindow.cc
@@ -16,12 +16,15 @@ QString app=APP_NAME " " VERSION;
 
 /** constructor of AboutWindow, creates window and fills it with elements, parameters are ignored */
 AboutWindow::AboutWindow(QWidget *parent/*=0*/,const char *name/*=0*/):QWidget(parent,name,WDestructiveClose || WType_TopLevel) {
- QString info=tr("PDF editor for unix systems");
- QString authors=tr("Project leader")+":\n Martin Beran\n"+tr("Authors")+":\n Michal Hocko\n Miro Jahoda\n Josef Misutka\n Martin Petricek\n";
+ QString info=QString("<big>")+tr("PDF editor for unix systems")+"</big>";
+ QString authors=QString("<b>")+tr("Project leader")+":</b><br>&nbsp; Martin Beran<br><b>"
+                               +tr("Authors")+":</b><br>&nbsp; Michal Hocko<br>&nbsp; Miro Jahoda<br>&nbsp; Josef Misutka<br>&nbsp; Martin Petricek<br>";
  setCaption(tr("About program"));
  QBoxLayout *l=new QVBoxLayout(this);
 // QVBox *qb=new QVBox(this,"about_vbox");
- QLabel *lb=new QLabel(app+"\n"+tr("Compiled")+": "+COMPILE_TIME+"\n\n"+info+"\n\n"+authors+"\n", this);
+ QLabel *lb=new QLabel(QString("<table><tr><td valign=\"top\"><h1>")+app+"</h1><br>"+tr("Compiled")+": "+COMPILE_TIME+"<br><br>"+
+  info+"</td><td valign=\"bottom\">"+authors+"</td></tr><tr><td colspan=\"\2\">"+tr("This program is distributed under terms of GNU GPL")+"</td></tr></table>", this);
+ lb->setTextFormat(Qt::RichText);
  QPushButton *ok=new QPushButton(tr("Ok"), this);
  QObject::connect(ok, SIGNAL(clicked()), this, SLOT(close()));
  l->addWidget(lb);
