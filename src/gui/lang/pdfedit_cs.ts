fe49e4f33f5a88135085a59c36e30be3b9205d28
Martin Petříček <tux@centrum.cz>
2006-04-01 13:09:33 +0000

    Added translations

---
+++ src/gui/lang/pdfedit_cs.ts
@@ -0,0 +1,106 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <source>PDF editor for unix systems</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Project leader</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Authors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About program</source>
+        <translation>O programu</translation>
+    </message>
+    <message>
+        <source>Compiled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This program is distributed under terms of GNU GPL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Null</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bool</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Int</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Real</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>String</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ref</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Array</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Dict</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stream</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DebugCmd</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DebugError</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DebugEOF</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>DebugNone</source>
+        <comment>Type name</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
