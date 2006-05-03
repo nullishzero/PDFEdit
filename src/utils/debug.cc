47b69f88576423bce051f1938e0bca7ace47cc7d
Martin Petříček <tux@centrum.cz>
2006-05-03 11:38:35 +0000

    default level changed to CRIT from PANIC

--- src/utils/debug.cc
+++ src/utils/debug.cc
@@ -1,9 +1,9 @@
 #include "debug.h"
 
 // if default debug level doesn't come from gcc command line, we use
-// PANIC
+// CRIT
 #ifndef DEFAULT_DEBUG_LEVEL
-#define DEFAULT_DEBUG_LEVEL debug::DBG_PANIC
+#define DEFAULT_DEBUG_LEVEL debug::DBG_CRIT
 #endif
 
 namespace debug
