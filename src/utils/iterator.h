52ae2695e65e736c097210271808588287418310
Jozef Mišutka <misutkajunior@hotmail.com>
2006-05-16 18:00:17 +0000

    -- improved

--- src/utils/iterator.h
+++ src/utils/iterator.h
@@ -10,6 +10,7 @@
 #define _ITERATOR_H_
 
 #include <exception>
+#include <boost/smart_ptr.hpp>
 
 //==========================================================
 namespace iterator {
@@ -25,66 +26,139 @@ struct IteratorInvalidObject : public std::exception
 /**
  * Iterator interface from Gof book. We do NOT want it to 
  * be like stl iterators, because we do NOT need all the functionality and also
- * prev and nex it is stored in the Item itself, not in a special container.
+ * prev and next are stored in the Item itself, not in a special container.
  *
- * The item over which to iterate has to implement 2 methods:
+ * The item type over which to iterate has to implement 2 methods:
  * <ul>
  * 	<li> _next () </li>
  * 	<li> _prev () </li>
  * </ul>
  * 
- * 
- * It could be confusing.
+ * Also an assumption is made, that we use something like weak smart pointers so we 
+ * must use lock method.
+ *
+ * This class has two template methods (Design pattern). It can be easily extended to specific
+ * iterators overloading isValidItem method.
  */
-template<typename Item>
-class LinkedListIterator
+template<typename ITEM>
+class SharedDoubleLinkedListIterator
 {
-private:
-	Item _cur;
-		
+public:
+	typedef boost::weak_ptr<ITEM> ListItem;
+
+protected:
+	ListItem _cur;
+
+	//
+	// Constructor
+	// 
 public:
 
 	/** Constructors. */
-	LinkedListIterator (Item oper) : _cur (oper) {};
-
-	/** Next. */
-	LinkedListIterator<Item>& next ()
-	{
-		if (!_cur.expired())
-		{
-			assert (_cur.lock());
-			_cur = _cur.lock()->_next ();
-					
-		}else
-			throw IteratorInvalidObject ();
-
-		return *this;
-	};
+	SharedDoubleLinkedListIterator (ListItem oper) : _cur (oper) {};
 	
-	/** Previous. */
-	LinkedListIterator<Item>& prev ()
+	/** 
+	 * Constructors. 
+	 * Be carefull when using this one. It does not point anywhere.
+	 */
+	SharedDoubleLinkedListIterator () : _cur (ListItem()) {};
+
+	//
+	// Destructor
+	//
+public:
+	virtual ~SharedDoubleLinkedListIterator () {};
+	
+	//
+	// Iterator interface
+	//
+public:
+	/** 
+	 * Next. 
+	 */
+	SharedDoubleLinkedListIterator<ITEM>& next ()
 	{
-		if (!_cur.expired())
-		{
-			assert (_cur.lock());
-			_cur = _cur.lock()->_prev ();
+		// Loop until an item is valid or end is reached
+		do {
+			if (!_cur.expired())
+			{
+				assert (_cur.lock());
+				_cur = _cur.lock()->_next ();
+						
+			}else
+			{// We are at the end
+				break;
+				//assert (!"the end++");
+				//throw IteratorInvalidObject ();
+			}
 			
-		}else
-			throw IteratorInvalidObject ();
-		
+		}while (!validItem());
+
 		return *this;
-	};
+	}
 	
-	/** Get current. */
-	Item getCurrent () const {return _cur;};
+	/** 
+	 * Previous. 
+	 */
+	SharedDoubleLinkedListIterator<ITEM>& prev ()
+	{
+		// Loop until an item is valid or end is reached
+		do {
+			if (!_cur.expired())
+			{
+				assert (_cur.lock());
+				_cur = _cur.lock()->_prev ();
+						
+			}else
+			{// We are at the end
+				break;
+				//assert (!"the end++");
+				//throw IteratorInvalidObject ();
+			}
 
-	/** Are we at the end. Doesn't matter whether in the front or at the back */
-	bool isEnd () const { return (_cur.expired()) ? true : false; };
+		}while (!validItem());
 
+		return *this;
+	}
+	
+	
+	//
+	// Comparable interface
+	//
+public:	
+	bool operator== (const SharedDoubleLinkedListIterator<ITEM>& it) const
+		{ return (it.getCurrent() == getCurrent());	}
+	
+	
+	//
+	// Helper methods
+	//
+public:
+	/** 
+	 * Get current. It returns locked current pointer. 
+	 */
+	boost::shared_ptr<ITEM> getCurrent () const {return _cur.lock();}
+
+	/** 
+	 * Are we at the end. Doesn't matter whether in the front or at the back 
+	 */
+	bool isEnd () const { return (_cur.expired()) ? true : false; }
+
+	
+	//
+	// Template method interface
+	//
+public:
+	/**
+	 * In this base iterator, loop through all objects.
+	 *
+	 * @return True if the item is supported by this type of iterator, false otherwise.
+	 */
+	virtual bool validItem () const {return true;}
 };
 
 //==========================================================
-}
+} // namespace iterator
 //==========================================================
 
 #endif // _ITERATOR_H_
