40afee658fee8a35091fe921e12f8ef540b44fae
Michal Hocko <mstsxfx@gmail.com>
2006-05-16 17:42:21 +0000

    * Delinearizator class implementation * NotLinearizedException added

---
+++ src/utils/delinearizator.cc
@@ -0,0 +1,195 @@
+// vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
+/*
+ * $RCSfile$
+ *
+ * $Log$
+ * Revision 1.1  2006/05/16 17:42:21  hockm0bm
+ * * Delinearizator class implementation
+ * * NotLinearizedException added
+ *
+ *
+ */
+
+#include <errno.h>
+#include "delinearizator.h"
+#include "debug.h"
+#include "kernel/streamwriter.h"
+#include "kernel/factories.h"
+
+
+using namespace pdfobjects;
+using namespace utils;
+
+Delinearizator::Delinearizator(FileStreamWriter * stream, IPdfWriter * writer)
+	:XRef(stream), 
+	pdfWriter(writer)
+{
+	if(getErrorCode() !=errNone)
+	{
+		// xref is corrupted
+		throw MalformedFormatExeption("XRef parsing problem errorCode="+getErrorCode());
+	}
+
+	if(!checkLinearized(*stream, this, &linearizedRef))
+		throw NotLinearizedException();
+}
+Delinearizator * Delinearizator::getInstance(const char * fileName, IPdfWriter * pdfWriter)
+{
+using namespace debug;
+	
+	utilsPrintDbg(DBG_DBG, "fileName="<<fileName);
+
+	// opens file handle and creates FileStreamWriter instance
+	FILE * file=fopen(fileName, "r");
+	if(!file)
+	{
+		int err=errno;
+		utilsPrintDbg(DBG_ERR, "Unable to open file. Error message="<<strerror(err));
+		return NULL;
+	}
+	Object dict;
+	FileStreamWriter * inputStream=new FileStreamWriter(file, 0, false, 0, &dict);
+
+	// creates instance
+	Delinearizator * instance;
+	try
+	{
+		instance=new Delinearizator(inputStream, pdfWriter);
+	}catch(std::exception & e)
+	{
+		utilsPrintDbg(DBG_ERR, "Unable to create Delinearizator instance. Error message="<<e.what());
+		return NULL;
+	}
+
+	return instance;
+
+}
+
+int Delinearizator::delinearize(const char * fileName)
+{
+using namespace debug;
+
+	utilsPrintDbg(DBG_DBG, "fileName="<<fileName);
+
+	FILE * file=fopen(fileName, "w");
+	int err=0;
+	if(!file)
+	{
+		err=errno;
+		utilsPrintDbg(DBG_ERR, "Unable to open file. Error message="<<strerror(err));
+		return err;
+	}
+	
+	// delegates and closes file
+	err=delinearize(file);
+	fclose(file);
+	return err;
+}
+
+int Delinearizator::delinearize(FILE * file)
+{
+using namespace debug;
+
+	utilsPrintDbg(DBG_DBG, "");
+	if(!file)
+	{
+		utilsPrintDbg(DBG_ERR, "Bad file handle");
+		// FIXME to constant
+		return -1;
+	}
+	if(!pdfWriter)
+	{
+		utilsPrintDbg(DBG_ERR, "No pdfWriter specified. Aborting");
+		// FIXME to constant
+		return -1;
+	}
+	
+	// stream from input file
+	StreamWriter * inputStream=dynamic_cast<StreamWriter *>(::XRef::str);
+
+	// creates outputStream writer from given file
+	Object dict;
+	StreamWriter * outputStream=new FileStreamWriter(file, 0, false, 0, &dict);
+	
+	// buffer for data
+	char buffer[1024];
+	memset(buffer, '\0', sizeof(buffer));
+	
+	// resets outputStream and sets position by Stream::getStart method to copies all 
+	// data until end of the line (first of CR or LF characters)
+	inputStream->reset();
+	inputStream->setPos(inputStream->getStart());
+	do
+	{
+		if(!inputStream->getLine(buffer, sizeof(buffer)-1))
+		{
+			// this should never happen
+			utilsPrintDbg(DBG_ERR, "File is corrupted because, doesn't contain proper PDF header.");
+			return -1;
+		}
+	}while(!strstr(buffer, PDFHEADER));
+
+	// prints header to the file
+	outputStream->putLine(buffer, strlen(buffer));
+
+	// PDF specification succests that header line should be followed by comment
+	// line with some binary (with codes bigger than 128) - so application
+	// transfering such files will copy them as binary data not as ASCII files
+	buffer[0]='%';
+	buffer[1]=129;
+	buffer[2]=130;
+	buffer[3]=253;
+	buffer[4]=254;
+	buffer[5]='\0';
+	outputStream->putLine(buffer, strlen(buffer));
+
+	// now collects (fetches) all objects from XRef::entries array which are not free to 
+	// the objectList
+	utilsPrintDbg(DBG_DBG, "Collecting all objects");
+	IPdfWriter::ObjectList objectList;
+	for(size_t i=0; i < (size_t)::XRef::size; i++)
+	{
+		::XRefEntry entry=::XRef::entries[i];
+		if(entry.type==xrefEntryFree)
+			continue;
+
+		::Object * obj=XPdfObjectFactory::getInstance();
+		int num=i, 
+			gen=entry.gen;
+		
+		// if entry is compressed (from object stream, gen is allways 0 but xpdf
+		// uses this number for object order in stream)
+		if(entry.type==xrefEntryCompressed)
+			gen=0;
+
+		// fetches object unless it is Linearized dicionary
+		if(num==linearizedRef.num && gen==linearizedRef.gen)
+			continue;
+		XRef::fetch(num, gen, obj);
+		::Ref ref={num, gen};
+		objectList.push_back(IPdfWriter::ObjectElement(ref, obj));
+	}
+
+	// writes collected objects and xref & trailer section
+	utilsPrintDbg(DBG_INFO, "Writing "<<objectList.size()<<" objects to the output outputStream.");
+	pdfWriter->writeContent(objectList, *outputStream);
+	utilsPrintDbg(DBG_INFO, "Writing xref and trailer section");
+	// no previous section information and all objects are going to be written
+	IPdfWriter::PrevSecInfo prevInfo={0, 0};
+	pdfWriter->writeTrailer(*getTrailerDict(), prevInfo, *outputStream);
+
+	// clean up
+	utilsPrintDbg(DBG_DBG, "Cleaning up all writen objects.");
+	for(IPdfWriter::ObjectList::iterator i=objectList.begin(); i!=objectList.end(); i++)
+	{
+		freeXpdfObject(i->second);
+		i->second=NULL;
+	}
+
+	outputStream->flush();
+	// deallocates outputStream
+	delete outputStream;
+
+	return 0;
+}
+
---
+++ src/utils/delinearizator.h
@@ -0,0 +1,168 @@
+// vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
+/*
+ * $RCSfile$
+ *
+ * $Log$
+ * Revision 1.1  2006/05/16 17:42:21  hockm0bm
+ * * Delinearizator class implementation
+ * * NotLinearizedException added
+ *
+ *
+ */
+
+#ifndef _DELINEARIZATOR_H_
+#define _DELINEARIZATOR_H_
+
+#include "kernel/xpdf.h"
+#include "kernel/exceptions.h"
+#include "kernel/pdfwriter.h"
+
+/** Exception for not linearized pdf documents.
+ * This exception is thrown if Delinearizator is used for non linearized
+ * documents.
+ *
+ */
+class NotLinearizedException:public PdfException
+{
+public:
+	virtual ~NotLinearizedException()throw()
+	{}
+
+	virtual const char * what()throw()
+	{
+		return "Content is not linerized";
+	}
+};
+
+namespace pdfobjects
+{
+
+namespace utils
+{
+
+/** Delinearizator class.
+ *
+ * Provides functionality enabling correct changing linearized pdf documents to
+ * their unlinearized form. This may be very usefull specially if chnages has to
+ * be done to document, because linearized document has rather strict rules and
+ * so can be easily broken by incremental changes.
+ * <p>
+ * Class reuses XRef class for low level pdf reading and parsing purposes
+ * and adds logic related to linearized documents.
+ * <p>
+ * Output file will contain all objects - except of those needed by linearized
+ * structure - in same format (e. g. filters in content streams), object and
+ * generation numbers. 
+ * <br>
+ * Linearize documents are not prepared for multiversion documents very well (as
+ * mentioned before) and so output file will contain just one trailer and xref
+ * (so one revision). Format of this final section fully depends on given
+ * IPdfWriter implementator (set in constructor).
+ * <p>
+ * <b>Usage</b>
+ * <br>
+ */
+class Delinearizator: protected ::XRef
+{
+	/** Pdf content writer implementator.
+	 *
+	 * All writing of pdf content is delegated to this field.
+	 */
+	IPdfWriter * pdfWriter;
+
+	/** Reference of linearized dictionary.
+	 */
+	::Ref linearizedRef;
+
+protected:
+	/** Initialization constructor.
+	 * @param stream FileStreamWriter instance.
+	 * @param writer Pdf content writer.
+	 *
+	 * Uses XRef(BaseStream *) constructor and initializes pdfWriter with given
+	 * one. 
+	 * 
+	 * @throw MalformedFormatExeption if file content is not valid pdf document.
+	 * @throw NotLinearizedException if file content is not linearized.
+	 */
+	Delinearizator(FileStreamWriter * stream, IPdfWriter * writer);
+public:
+	/** Destructor.
+	 *
+	 * Destroys XRefWriter internal data, deallocates pdfWriter and closes file
+	 * handle opened used for FileStre.
+	 */
+	~Delinearizator()
+	{
+		if(pdfWriter)
+			delete pdfWriter;
+	}
+	
+	/** Factory method for isntance creation.
+	 * @param fileName Name of the pdf file.
+	 * @param pdfWriter  Pdf content writer.
+	 *
+	 * Creates FileStream from given fileName (file is open with `r' mode) and 
+	 * creates Delinearizator instance. Finaly checks whether file is 
+	 * linearized and if not, prints error message and returns with NULL.
+	 *
+	 * @throw MalformedFormatExeption if file content is not valid pdf document.
+	 * @return Delinearizator instance ready to be used or NULL, if given file
+	 * is not linearized.
+	 */
+	static Delinearizator * getInstance(const char * fileName, IPdfWriter * pdfWriter);
+
+	/** Sets new pdf content writer.
+	 * @param pdfWriter IPdfWriter interface implementator.
+	 *
+	 * If given parameter is not NULL, sets new value of pdfWriter field and
+	 * returns an old one. Otherwise just returns current one.
+	 * <br>
+	 * NOTE that caller is responsible for deallocation if provides new one.
+	 *
+	 * @return Currently set implementator or old value if parameter is non NULL.
+	 */
+	IPdfWriter * setPdfWriter(IPdfWriter * pdfWriter)
+	{
+		IPdfWriter *current=pdfWriter;
+		
+		if(pdfWriter)
+			this->pdfWriter=pdfWriter;
+
+		return current;
+	}
+	
+	/** Delinearizes pdf content.
+	 * @param fileName Name of the output file.
+	 *
+	 * Opens given file (in trucate mode) and delegates the rest to 
+	 * delinearize(FILE *) method. If given file doesn't exist, it will be
+	 * created. Finally closes file.
+	 *
+	 * @see delinearize(FILE *)
+	 *
+	 * @return 0 if everything ok, otherwise value of errno of the error.
+	 */
+	int delinearize(const char * fileName);
+	
+	/** Delinearizes pdf content.
+	 * @param file File handle where to put data.
+	 *
+	 * Sets position to the file beginning and writes same pdf header as in
+	 * original stream. Then starts copying of all objects - except Linearization
+	 * dictionary - which are available in XRef::entries array.
+	 * Finaly stores xref and trailer section.
+	 * <br>
+	 * Caller is responsible for file opening and closing.
+	 * <br>
+	 * NOTE that method doesn't check whether given file is same as one used for
+	 * input data. If it refers to same file, result is unpredictable.
+	 *
+	 * @return 0 if everything ok, otherwise valie of error of the error.
+	 */
+	int delinearize(FILE * file);
+};
+
+} // end of namespace utils
+} // end of pdfobjects namespace
+#endif
