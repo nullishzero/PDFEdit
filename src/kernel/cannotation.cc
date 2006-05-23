7838ce7eb692aa7be558d61f2146256fcd16d8a9
Michal Hocko <mstsxfx@gmail.com>
2006-05-23 19:12:03 +0000

    * IAnnotInitializator added * UniversalAnnotInitializer implemented * TextAnnotInitializer         - skeleton implementation * CAnnotation         - static initializer         - constructor with initializer         - constructor with initialized dictionary

--- src/kernel/cannotation.cc
+++ src/kernel/cannotation.cc
@@ -4,6 +4,16 @@
  * $RCSfile$ 
  *
  * $Log$
+ * Revision 1.2  2006/05/23 19:12:03  hockm0bm
+ * * IAnnotInitializator added
+ * * UniversalAnnotInitializer implemented
+ * * TextAnnotInitializer
+ *         - skeleton implementation
+ * * CAnnotation
+ *         - static initializer
+ *         - constructor with initializer
+ *         - constructor with initialized dictionary
+ *
  * Revision 1.1  2006/05/11 21:05:48  hockm0bm
  * first scratch of CAnnotation class
  *         - still not in kernel.pro
@@ -13,27 +23,139 @@
 #include"cannotation.h"
 #include"debug.h"
 #include"factories.h"
+#include"cobjecthelpers.h"
 
 using namespace boost;
+using namespace std;
 
 namespace pdfobjects {
 
+namespace utils {
+
+UniversalAnnotInitializer::SupportedList UniversalAnnotInitializer::getSupportedList()const
+{
+	// returns list with names from implList
+	SupportedList list;
+	for(AnnotList::const_iterator i=implList.begin(); i!=implList.end(); i++)
+	{
+		string annotType=i->first;
+		list.push_back(annotType);
+	}
+
+	return list;
+}
+
+bool UniversalAnnotInitializer::operator()(boost::shared_ptr<CDict> & annotDict, std::string annotType)
+{
+	// checks if given annotType is supported
+	AnnotList::const_iterator implElem;
+	if((implElem=implList.find(annotType))==implList.end())
+		return false;
+	shared_ptr<IAnnotInitializator> impl=implElem->second;
+
+	// use registerd implementator
+	return (*impl)(annotDict, annotType);
+}
+
+bool UniversalAnnotInitializer::registerInitializer(std::string annotType, shared_ptr<IAnnotInitializator> impl, bool forceNew)
+{
+	AnnotList::iterator elem=implList.find(annotType);
+	if(elem!=implList.end())
+	{
+		if(!forceNew)
+			// keep existing mapping
+			return false;
+
+		// this annotation type is already registered but we are forced to
+		// rewrite implementation
+		elem->second=impl;
+		return true;
+	}
+	
+	// this mapping is not registered yet
+	implList.insert(AnnotList::value_type(annotType, impl));
+	return true;
+}
+
+/** Checks and replaces mapping in given dictionary.
+ * @param annotDict Dictionary to check.
+ * @param fieldName Field name to search.
+ * @param fieldValue New value for this fieldName.
+ *
+ * Sets new value of given fieldName field and returns true if this property was
+ * replaced, false if it was added.
+ *
+ * @return true if given fieldName's value has been overwirten, false otherwise.
+ */
+bool checkAndReplace(shared_ptr<CDict> annotDict, string fieldName, IProperty & fieldValue)
+{
+	shared_ptr<IProperty> value;
+	try
+	{
+		value=annotDict->getProperty(fieldName);
+		annotDict->setProperty(fieldName, fieldValue);
+		return true;
+	}catch(ElementNotFoundException &e)
+	{
+		// annotDict doesn't contain fieldName so one has to be added
+		annotDict->addProperty(fieldName, fieldValue);
+		return false;
+	}
+}
+
+IAnnotInitializator::SupportedList TextAnnotInitializer::getSupportedList()const
+{
+	SupportedList list;
+	list.push_back("Text");
+
+	return list;
+}
+
+// initializes static constant
+bool TextAnnotInitializer::OPEN=true;
+
+bool TextAnnotInitializer::operator()(boost::shared_ptr<CDict> & annotDict, std::string annotType)const
+{
+	if(annotType!="Text")
+		return false;
+
+	IProperty * defaultSubType=CNameFactory::getInstance("Text");
+	checkAndReplace(annotDict, "Subtype", *defaultSubType);
+	delete defaultSubType;
+
+	IProperty * defaultOpen=CBoolFactory::getInstance(OPEN);
+	checkAndReplace(annotDict, "Open", *defaultOpen);
+	delete defaultOpen;
+
+	return true;
+}
+
+} // namespace utils
+
+// initialization of static
+shared_ptr<utils::IAnnotInitializator> 
+CAnnotation::annotInit=shared_ptr<utils::IAnnotInitializator>(new utils::UniversalAnnotInitializer());
+
 // TODO page has to be valid (pdf and reference) because annotDict is stored as
 // reference inside Annots array. Meaningfull reference needs PDF
-CAnnotation::CAnnotation(shared_ptr<CDict> annotDict, shared_ptr<CDict> page):annotDictionary(annotDict), valid(true)
+CAnnotation::CAnnotation(shared_ptr<CDict> annotDict, shared_ptr<CDict> page, string annotType)
+	:valid(true)
 {
 using namespace debug;
 using namespace utils;
 
 	kernelPrintDbg(DBG_DBG, "");
 	
-	// page dictionary has to have valid pdf, because annotations are indirect
-	// objects available by reference in Annots page dictionart
-	if(isInValidPdf(*page))
+	// page dictionary has to have valid pdf and reference, because 
+	// annotations are indirect objects available by reference in 
+	// Annots page dictionart
+	if(!hasValidRef(page) || !hasValidPdf(page))
 	{
 		kernelPrintDbg(DBG_ERR, "Page dictionary is not in pdf. ");
+		valid=false;
 		// TODO handle	
 	}
+	CPdf * pdf=page->getPdf();
 	
 	shared_ptr<CArray> annotArray;
 
@@ -63,23 +185,37 @@ using namespace utils;
 	// if annotDict doesn't belong to same pdf as page, adds it as indirect 
 	// property to same pdf
 	IndiRef annotRef=annotDict->getIndiRef();
-	if(isInValidPdf(annotDict))
+	if(!hasValidPdf(annotDict))
 		annotRef=pdf->addIndirectProperty(annotDict);
 	
-	CRef annotCRef(annotDict);
+	// checks whether annotation's reference is in Annots array. If not inserts
+	// it.
+	shared_ptr<CRef> annotCRef(CRefFactory::getInstance(annotRef));
 	vector<CArray::PropertyId> container;
 	getPropertyId(annotArray, annotCRef, container);
 	if(!container.size())
 	{
 		// adds annotation reference to the array
-		// TODO may be invalid reference and then addIndirectProperty has to be
-		// called
 		// This will produce annotation observer notification
 		kernelPrintDbg(DBG_DBG, "AnnotDict not found in Annots array. Appending to the pos="<<annotDict->getPropertyCount());
 		CRef annotCRef(annotRef);
 		annotArray->addProperty(annotCRef);
 	}
 	
+	// annotation dictionary is in pdf now and also page contains reference to
+	// it. We can resolve it from annotRef
+	shared_ptr<IProperty> annotProp=pdf->getIndirectProperty(annotRef);
+	this->annotDictionary=IProperty::getSmartCObjectPtr<CDict>(annotProp);
+
+	// initializes annotation dictionary according desired type - at least Type
+	// and Subtype are initialized if initializator implementation doesn't
+	// support given type
+	bool initialized=false;
+	if(annotInit)
+		initialized=(*annotInit)(this->annotDictionary, annotType);
+
+	if(!initialized)
+		kernelPrintDbg(DBG_WARN, "Unable to initialize annotation dictionary with type="<<annotType);
 }
 
 } // end of pdfobjects namespace
--- src/kernel/cannotation.h
+++ src/kernel/cannotation.h
@@ -4,6 +4,16 @@
  * $RCSfile$ 
  *
  * $Log$
+ * Revision 1.3  2006/05/23 19:12:03  hockm0bm
+ * * IAnnotInitializator added
+ * * UniversalAnnotInitializer implemented
+ * * TextAnnotInitializer
+ *         - skeleton implementation
+ * * CAnnotation
+ *         - static initializer
+ *         - constructor with initializer
+ *         - constructor with initialized dictionary
+ *
  * Revision 1.2  2006/05/11 21:05:48  hockm0bm
  * first scratch of CAnnotation class
  *         - still not in kernel.pro
@@ -17,7 +27,158 @@
 #include "static.h"
 #include "cobject.h"
 
-using namespace pdfobjects;
+namespace pdfobjects
+{
+
+namespace utils
+{
+
+/** Annotation initializator interface.
+ *
+ * Provides interface for initializators of annotation dictionaries.
+ * Implementator should return all supported annotation types by
+ * getSupportedList method. Function operator initializes given annotation
+ * dictionary.
+ * <br>
+ * This is implementation of stateless builder design pattern (stateless means
+ * that result is constructed in one step and returned directly from construct
+ * function operator).
+ */
+class IAnnotInitializator
+{
+public:
+	typedef std::vector<std::string> SupportedList;
+
+	virtual ~IAnnotInitializator(){};
+	
+	/** Returns list of supported annotation types.
+	 *
+	 * Returned container contains all supprted annotation types, which can be
+	 * used in function operator to make some initialization.
+	 * 
+	 * @return SupportedList container with all supprted types.
+	 */
+	virtual SupportedList getSupportedList()const =0;
+
+	/** Initialization (builder) function.
+	 * @param annotDict Annotation dictionary to initialize.
+	 * @param annotType Name of the annotation type to initialize.
+	 *
+	 * Initializes given dictionary to given annotType annotation type. If
+	 * doesn't support such type, doesn't do any special initialization and
+	 * returns with false.
+	 * <br>
+	 * In any case checks whether given annotDict contains Type field with /Annot
+	 * value and if not sets this value.
+	 * <br>
+	 * Implemntator may do some checking and fail if given annotType can't be
+	 * initialized (for example if it is initialized as different type which is
+	 * in conflict with desired one).
+	 
+	 * @return if given annotType special initialization was done for annotDict
+	 */
+	virtual bool operator()(boost::shared_ptr<CDict> & annotDict, std::string annotType)=0;
+};
+
+/** Composite for annotation intializers.
+ *
+ * Collects implementators of IAnnotInitializator interfaces. Each implementator
+ * is associated with annotation type(s) which is supported. Function operator
+ * delegates work to registered implementator or just adds Type field with Annot
+ * value.
+ */
+class UniversalAnnotInitializer: public IAnnotInitializator
+{
+public:
+	typedef std::map<std::string, boost::shared_ptr<IAnnotInitializator> > AnnotList;
+private:
+	AnnotList implList;
+public:
+	/** Empty constructor.
+	 *
+	 * Doesn't do any special initialization. implList is empty and so no
+	 * initializators are supported by default.
+	 */
+	UniversalAnnotInitializer(){}
+	
+	/** Initialization constructor from exiting mapping.
+	 * @param impls Existing mapping.
+	 *
+	 * Use given mapping for implList initialization.
+	 */
+	UniversalAnnotInitializer(AnnotList impls):implList(impls){}
+
+	virtual ~UniversalAnnotInitializer(){};
+
+	/** Returns list of supported initializators.
+	 * 
+	 * @return list of all names from implList.
+	 */
+	SupportedList getSupportedList()const;
+		
+	/** Registers new implementator for given annotation type.
+	 * @param annotType Type of the annotation.
+	 * @param impl Implementator for initializator.
+	 * @param forceNew Flag controling overwriting.
+	 *
+	 * Checks whether given type is already registered (mapping for this key is
+	 * in implList) and if it is checks forceNew flag. If it is true, replaces
+	 * current implementator (in implList) with given one, otherwise doesn't do
+	 * anything. If it is not registered, registers new with annotType as key
+	 * and impl pair to implList.
+	 *
+	 * @return true if implementation was changed/added, false otherwise.
+	 */
+	bool registerInitializer(std::string annotType, boost::shared_ptr<IAnnotInitializator> impl, bool forceNew=false);
+
+	/** Initialization function.
+	 * @param annotDict Annotation dictionary to initialize.
+	 * @param annotType Name of the annotation type to initialize.
+	 *
+	 * Sets Type field value to Annot, if it as different or missing value and
+	 * searches initializator for given annotType. If found, delegates to the
+	 * implementation and returns same as implementator functor. Otherwise
+	 * returns false.
+	 * @return if given annotType special initialization was done for annotDict.
+	 */
+	virtual bool operator()(boost::shared_ptr<CDict> & annotDict, std::string annotType);
+
+};
+
+/** Initializator for Text annotation.
+ *
+ */
+class TextAnnotInitializer: public IAnnotInitializator
+{
+public:
+	/** Default value for Open entry.
+	 * Value is true by default.
+	 */
+	static bool OPEN;
+	
+	/** Returns supported type.
+	 *
+	 * @return list with one element with Text type.
+	 */
+	IAnnotInitializator::SupportedList getSupportedList()const;
+
+	/** Initializes text annotation.
+	 * @param annotDict Annotation dictionary to initialize.
+	 * @param annotType Name of the annotation type to initialize (must be Text).
+	 *
+	 * Checks if given annotType is Text and if yes, initializes given
+	 * dictionary as Text annotation. Otherwise immediately returns with false.
+	 * <br>
+	 * Initialization doesn't do any checking and so annotation dictioanries
+	 * initialized to different type shouldn't be used here. Result may be
+	 * incorrect annotation dictionary in such situation.
+	 *
+	 * @return true if initialization is successfull, false otherwise.
+	 */
+	virtual bool operator()(boost::shared_ptr<CDict> & annotDict, std::string annotType)const;
+};
+
+} // namespace utils
 
 /** High level object for annotations.
  *
@@ -55,18 +216,30 @@ class CAnnotation
 	/** Validity flag.
 	 */
 	bool valid;
+
+	/** Initializator for annotations.
+	 * This implementator is used when annotation dictionary should be
+	 * initialized.
+	 * Concrete implementation can be set by setAnnotInitializator method.
+	 */
+	static boost::shared_ptr<utils::IAnnotInitializator> annotInit;
 public:
 	/** Initialization constructor.
 	 * @param annotDict Annotation dictionary.
-	 * @param page Page dictionary where this annotation is/should be
 	 *
-	 * Initializes annotDictionary field and set valid flag to true.
-	 * Checks whether given dictionary is available from given page dictionary.
-	 * If not adds it to the page's Annots array.
+	 * Initializes annotDictionary from given one and sets valid flag to true.
+	 * Doesn't perform any checking.
 	 * <br>
-	 * valid field is allways set to true.
+	 * This should be used when caller is sure that given annotDict is ok (e. g.
+	 * when dictionary is created from template)
 	 */
-	CAnnotation(boost::shared_ptr<CDict> annotDict, boost::shared_ptr<CDict> page);
+	CAnnotation(boost::shared_ptr<CDict> annotDict):annotDictionary(annotDict), valid(true){}
+	
+	/** Initialization constructor.
+	 */
+	CAnnotation(boost::shared_ptr<CDict> annotDict, 
+			boost::shared_ptr<CDict> page, 
+			std::string annotType);
 
 	/** Destructor.
 	 *
@@ -77,6 +250,27 @@ public:
 	{
 	}
 
+	/** Sets new annotation initializator.
+	 * @param init Initializator to be used.
+	 *
+	 * Static method which sets new implementator for annotation initializator.
+	 * If given init holds non NULL initializator, sets it to the static
+	 * annotInit field. 
+	 *
+	 * @return previous implementation.
+	 */
+	static boost::shared_ptr<utils::IAnnotInitializator> 
+		setAnnotInitializator(boost::shared_ptr<utils::IAnnotInitializator> init)
+	{
+		boost::shared_ptr<utils::IAnnotInitializator> old=annotInit;
+
+		// if given is un NULL, sets new value
+		if(init.get())
+			annotInit=init;
+
+		return old;
+	}
+	
 	/** Returns valid flag value.
 	 *
 	 * If this method returns false, all methods which would produce changes 
@@ -88,10 +282,9 @@ public:
 	{
 		return valid;
 	}
-
-	
 };
 
+} // namespace pdfobjects
 
 
 #endif // __CANNOTATION_H__
