9e9f10beefb37500bef2d56ab67e1533edaff26a
Michal Hocko <mstsxfx@gmail.com>
2006-05-31 20:10:22 +0000

    * PropertyMode enhanced and documented * ModeRule, ModeMatcher, ModeConfigurationParser classes added         - used by Modecontroller * New Modecontroller implementation         - IModeController removed         - enherits from ModeRulesManager         - setDefaultMode method added         - loadFromFile added - not implemented yet (parser ready)

--- src/kernel/modecontroller.cc
+++ src/kernel/modecontroller.cc
@@ -5,6 +5,16 @@
  * $RCSfile$
  *
  * $Log$
+ * Revision 1.6  2006/05/31 20:10:22  hockm0bm
+ * * PropertyMode enhanced and documented
+ * * ModeRule, ModeMatcher, ModeConfigurationParser classes added
+ *         - used by Modecontroller
+ * * New Modecontroller implementation
+ *         - IModeController removed
+ *         - enherits from ModeRulesManager
+ *         - setDefaultMode method added
+ *         - loadFromFile added - not implemented yet (parser ready)
+ *
  * Revision 1.5  2006/05/31 07:06:24  hockm0bm
  * doc update and prepared to be implemented
  *         - just for sync
@@ -20,53 +30,94 @@
 // =====================================================================================
 using namespace std;
 
-//
-//
-//
-ModeController&
-ModeController::Instance (const std::string& fileName)
+bool ModeMatcher::operator()(const ModeRule & original, const ModeRule & rule,  priority_t * priority)const
 {
-	// Create the object and get modes
-	static ModeController obj (fileName);
+	// most general if original is empty
+	if(original.type=="" && original.name=="")
+	{
+		if(priority)
+			*priority=PRIO0;
+		return true;
+	}
+
+	// type global rule
+	if(original.type!="" && original.name=="")
+	{
+		if(original.type!=rule.type)
+			return false;
+
+		// name matches
+		if(priority)
+			*priority=PRIO1;
+		return true;
+	}
+
+	// name global rule
+	if(original.type=="" && original.name!="")
+	{
+		if(original.name!=rule.name)
+			return false;
+
+		if(priority)
+			*priority=PRIO2;
+		return true;
+	}
+
+	// type, name specific rule
+	if(original.equals(rule))
+	{
+		if(priority)
+			*priority=PRIO3;
+		return true;
+	}
 	
-	return obj;
-}
-
-//
-//
-//
-ModeController::ModeController (const std::string& /*fileName*/) 
-{
-		utilsPrintDbg (debug::DBG_INFO, "Mode controller created.");
-
-		//
-		// get the modes
-		// 	?xml 
-		// 		-- QT
-		// 			-- dom
-		// 			-- sax
-		// 	?plain file
-		//
+	// doesn't match
+	return false;
 }
 
 
-//
-//
-//
-PropertyMode
-ModeController::getMode (const string& /*type*/, const string& /*name*/) const
+bool ModeConfigurationParser::parse(ModeRule & rule, PropertyMode & mode)
 {
-	//
-	// find it and return item.second
-	//
+using namespace std;
 	
-	return mdUnknown;
+	string ruleString;
+	string modeString;
+
+	baseParser.parse(ruleString, modeString);
+
+	// sets mode - string must be same as enum name
+	if(modeString=="mdUnknown")
+		mode=mdUnknown;
+	else
+		if(modeString=="mdNormal")
+			mode=mdNormal;
+		else
+			if(modeString=="mdHidden")
+				mode=mdHidden;
+			else
+				if(modeString=="mdAdvanced")
+					mode=mdAdvanced;
+				else
+					// unknown type of mode
+					return false;
+	
+	// converts ruleString to ModeRule structure
+	// type is the first part of string until `.' character and everything
+	// behind is name - if deliminer is not found name is empty
+	size_t dotPos=ruleString.find_first_of('.');
+	if(dotPos!=string::npos)
+	{
+		// separates
+		rule.type.assign(ruleString, 0, dotPos);
+		rule.name.assign(ruleString, dotPos, ruleString.length()-dotPos);
+	}else
+	{
+		// no deliminer
+		rule.type=ruleString;
+		rule.name="";
+	}
+
+	// TODO trim strings
+
+	return true;
 }
-
-
-
-
-
-
-
-
--- src/kernel/modecontroller.h
+++ src/kernel/modecontroller.h
@@ -5,6 +5,16 @@
  * $RCSfile$
  *
  * $Log$
+ * Revision 1.7  2006/05/31 20:10:22  hockm0bm
+ * * PropertyMode enhanced and documented
+ * * ModeRule, ModeMatcher, ModeConfigurationParser classes added
+ *         - used by Modecontroller
+ * * New Modecontroller implementation
+ *         - IModeController removed
+ *         - enherits from ModeRulesManager
+ *         - setDefaultMode method added
+ *         - loadFromFile added - not implemented yet (parser ready)
+ *
  * Revision 1.6  2006/05/31 07:06:24  hockm0bm
  * doc update and prepared to be implemented
  *         - just for sync
@@ -15,21 +25,200 @@
 #define _MODECONTROLLER_H_
 
 #include "static.h"
+#include "utils/rulesmanager.h"
+#include "utils/confparser.h"
 
 //=====================================================================================
 
 /** 
  * Enum describing property mode. 
- *
+ * 
  */
 enum PropertyMode
 {
-	mdUnknown, mdNormal, mdHidden, mdReadOnly, mdAdvanced
+	/** Unknown mode.
+	 */
+	mdUnknown, 
+	
+	/** Normal mode.
+	 * Should be visible and read write. Normal property.
+	 */
+	mdNormal, 
+	
+	/** Hidden mode.
+	 * Should not be visible but read write (when it is shown). Property is not
+	 * very important to see (just technical detail) but when it is visible, it
+	 * should be changeable.
+	 */
+	mdHidden, 
+	
+	/** Read-only mode.
+	 * Should not be editable when it is visible. It doesn't make much sense to
+	 * change such properties.
+	 */
+	mdReadOnly, 
+	
+	/** Advanced mode.
+	 * Should not be visible and editable. It is very dangeroous to change such
+	 * properties.
+	 */
+	mdAdvanced
 };
 
+/** Rule structure for mode.
+ * This type is used for RulesManager RuleType in ModeController.
+ *
+ * @see RulesManager
+ * @see ModeController
+ */
+struct ModeRule
+{
+	/** Complex value type.
+	 */
+	std::string type;
+
+	/** Field name in complex type.
+	 */
+	std::string name;
+
+	/** Comparator method.
+	 * @param rule Rule to compare with.
+	 *
+	 * Given rule is same as given one if its type and name fields are same as
+	 * these.
+	 *
+	 * @return true if given rule is same, false otherwise.
+	 */
+	bool equals(const ModeRule & rule)const
+	{
+		return type==rule.type && name==rule.name;
+	}
+};
+
+/** Typedef to IRuleMatcher for ModeMatcher.
+ */
+typedef rulesmanager::IRuleMatcher<ModeRule> IModeMatcher; 
+
+/** Implementator of IRuleMatcher for ModeRule.
+ *
+ * See functor operator for more precise information about mode rules
+ * matching evaluation.
+ */
+class ModeMatcher: public IModeMatcher
+{
+public:
+	typedef IModeMatcher::priority_t priority_t;
+	
+	/** Priority for weakest match.
+	 */
+	static const priority_t PRIO0=0;
+
+	/** Priority for type only match.
+	 */
+	static const priority_t PRIO1=1;
+
+	/** Priority for name only match.
+	 */
+	static const priority_t PRIO2=2;
+	
+	/** Priority for full match.
+	 */
+	static const priority_t PRIO3=3;
+
+	/** Determines whether given rule matches given original.
+	 * @param original Original rule.
+	 * @param rule Rule to check whether matches.
+	 * @param priority Place where to store priority of match (only if non NULL
+	 * and matches).
+	 *
+	 * <b>Rules</b>:
+	 * <ul>
+	 * <li>original={"", ""} - rule allways matches with PRIO0 priority.
+	 * <li>original={type, ""} - rule matches if rule.type==original.type with
+	 * PRIO1 priority.
+	 * <li>original={"", name} - rule matches if rule.name==original.name with
+	 * PRIO2 priority.
+	 * <li>original={type, name} - rule matches if original.equals(rule) with
+	 * PRIO3 priority.
+	 * <li> PRIO0 &lt PRIO1 &lt PRIO2 &lt PRIO3
+	 * </ul>
+	 *
+	 * @return true if given rule matches original, false otherwise.
+	 */
+	virtual bool operator()(const ModeRule & original, const ModeRule & rule,  priority_t * priority)const;
+};
+
+/** Typedef to RulesManager for ModeController.
+ */
+typedef rulesmanager::RulesManager<ModeRule, PropertyMode> ModeRulesManager;
+
+/** Simple decorator to IConfigurationParser which is able to provide key value
+ * strings.
+ *
+ * This basic implementation is initialized in constructor and all parsing work
+ * from input data is delagated to it. Returned result is just converted to
+ * ModeRule and PropertyMode.
+ */
+class ModeConfigurationParser: public configuration::IConfigurationParser<ModeRule, PropertyMode>
+{
+public:
+	typedef configuration::IConfigurationParser<std::string, std::string> StringParser;
+private:
+	/** Low level string parser.
+	 * This parser is initialized in constructor and it is used for input data
+	 * parsing.
+	 */
+	StringParser & baseParser;
+public:
+	/** Initialization constructor.
+	 * Initializes baseParser field and configure it to have correct blanksSet,
+	 * commentsSet and deliminerSet.
+	 */
+	ModeConfigurationParser(StringParser & parser):baseParser(parser)
+	{}
+
+	/** Skips current key, value pair.
+	 * Delegates to baseParser.skip()
+	 */
+	void skip()
+	{
+		baseParser.skip();
+	}
+
+	/** Checks whether we are on end of data.
+	 *
+	 * Delegates to baseParser.eod()
+	 *
+	 * @return true if there is nothing more to read, false otherwise.
+	 */
+	bool eod()
+	{
+		return baseParser.eod();
+	}
+
+	/** Parses key, value pair and transforms them to rule and mode.
+	 * @param rule Reference where to put parsed rule.
+	 * @param mode Reference where to put parsed mode.
+	 *
+	 * Delegates to baseParser.parse and transforms returned strings to  rule
+	 * and mode.
+	 * <p>
+	 * Rule string has format:
+	 * <pre>
+	 * type[.name]
+	 * </pre>
+	 * If `.' is not found name or there are no characters behind, name is
+	 * empty. Everythign before `.' is Type. It can also be empty. Both parts
+	 * are trimed before set (all leading and trailing blanks re skiped).
+	 * <br>
+	 * Name string should be same as PropertyMode names. If not recognized,
+	 * returns with an error.
+	 */
+	bool parse(ModeRule & rule, PropertyMode & mode);
+};
 
 /**
- * Interface for mode controller. 
+ * Basic mode controller implementation. 
  *
  * Provides mapping from type, name pair to Property mode, where type stands for
  * complex type name (Value of complex - dictionary - Type field value) and name
@@ -96,26 +285,81 @@ enum PropertyMode
  * <li>type, name are case sensitive.
  * </ul>
  *
- * This interface doesn't specify how to set rules, because it is designed for
- * consumers.
  */
-class IModeController
+class ModeController: protected ModeRulesManager
 {
+	/** Default mode.
+	 */
+	PropertyMode defaultMode;
+
+	/** Matcher for rules.
+	 */
+	ModeMatcher matcher;
 public:
+
+	typedef ModeConfigurationParser ConfParser;
+	
+	/** Constructor.
+	 *
+	 * Intiailizes defaultMode to mdUnknown.
+	 * Sets ModeMatcher instance to ModeRulesManager.
+	 */
+	ModeController():defaultMode(mdUnknown)
+	{ 
+		// initializes specialized rules matcher for property modes.
+		setRuleMatcher(&matcher);
+	};
+
+	/** Constructor with default mode.
+	 * @param defMod Default mode to be used.
+	 *
+	 * Initializes defaultMode with given one.
+	 * Sets ModeMatcher instance to ModeRulesManager.
+	 */
+	ModeController(PropertyMode defMod):defaultMode(defMod)
+	{
+		// initializes specialized rules matcher for property modes.
+		setRuleMatcher(&matcher);
+	};
+
+	/** Virtual destructor.
+	 * This method is empty, because no special treatment is required.
+	 */
+	virtual ~ModeController () {};
+
+	/** Reads given configuration file.
+	 * @param confFile Configuration file name.
+	 * @param parser Parser to be used for file parsing.
+	 *
+	 * Uses given parser to get key (ModeRule) and value (PropertyMode).
+	 * Those are registered using addRule inherited method.
+	 */
+	int loadFromFile(std::string confFile, ConfParser & parser);
+
 	/** 
 	 * Get default mode.
 	 * 
 	 * Default policy for non matching rules.
-	 * @return Default mode.
+	 * @return defaultMode field value.
 	 */
-	virtual PropertyMode getDefaultMode() const = 0;
+	virtual PropertyMode getDefaultMode() const
+	{
+		return defaultMode;
+	}
 
 	/** Sets default mode.
 	 * @param mode New default mode.
 	 *
 	 * @return Previous value of default mode.
 	 */
-	//virtual PropertyMode setDefaultMode(PropertyMode mode)=0;
+	virtual PropertyMode setDefaultMode(PropertyMode mode)
+	{
+		PropertyMode old=defaultMode;
+
+		defaultMode=mode;
+
+		return old;
+	}
 
 	/**
 	 * Get mode for value with given type and name.
@@ -128,56 +372,18 @@ public:
 	 * 
 	 * @return Mode which is most specific for given type, name pair.
 	 */
-	virtual PropertyMode getMode (const std::string& type, const std::string& name) const = 0;
-protected:
-	/** Destructor. */
-	virtual ~IModeController () {};
+	virtual PropertyMode getMode (const std::string& type, const std::string& name) const
+	{
+		ModeRule rule={type, name};
+		PropertyMode mode;
+
+		// delegates to ModeRulesManager and uses returned mode
+		if(findMatching(rule, &mode))
+			return mode;
+
+		// didn't match - defaultMode is used
+		return defaultMode;
+	}
 };
-
-/**
- * Default mode controller provided by kernel.
- *
- * It implements singleton idiom. (Meyers singleton)
- * 
- */
-class ModeController : public IModeController
-{
-typedef std::map<std::string, PropertyMode> Modes;
-
-private:
-	Modes modes;
-
-public:
-	/**
-	 * Create the object.
-	 */
-	static ModeController& Instance (const std::string& fileName);
-
-	// Get default mode
-	virtual PropertyMode getDefaultMode () const {return mdUnknown;}
-	
-	// Get mode
-	virtual PropertyMode getMode (const std::string& type, 
-								  const std::string& name) const;
-	
-	//
-	// Singleton
-	//
-private:
-	
-	/**
-	 * Parse the file for known properties.
-	 *
-	 * @param fileName File name.
-	 */
-	ModeController (const std::string& fileName);
-	
-private:
-	ModeController (const ModeController&);
-	ModeController& operator= (const ModeController&);
-	~ModeController () {utilsPrintDbg (debug::DBG_INFO, "Mode controller destroyed.");};
-};
-
-
 
 #endif  //_MODECONTROLLER_H_
