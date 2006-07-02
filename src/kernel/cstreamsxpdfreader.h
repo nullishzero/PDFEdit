5b5757316e290199257a9aaafdd627cf842f521d
Jozef Mišutka <misutkajunior@hotmail.com>
2006-07-02 14:08:12 +0000

    -- Problem: streams separated in crazy places (" fasfdas [" and " (fdas) (fads) ]") 	-- this cannot be parsed using only one stream at a time (xpdf does not return valid error code/object/...) 	-- used xpdf way when parsing (hopefully this changes in future)

--- src/kernel/cstreamsxpdfreader.h
+++ src/kernel/cstreamsxpdfreader.h
@@ -16,12 +16,161 @@
 #include "iproperty.h"
 // Filters
 #include "cstream.h"
+#include "cobjecthelpers.h"
 
+/** 
+ * Problem only cstream use is that streams can be separated in crazy
+ * places e.g. first stream: "... ["	second stream: " fdas  as fs]"
+ * and xpdf does not return valid objects when stream ends in the middle of
+ * an object.
+ */
+#define CSTREAMSREADER_USE_XPDF	1
 
 //=====================================================================================
 namespace pdfobjects {
 //=====================================================================================
 
+#if CSTREAMSREADER_USE_XPDF
+
+/**
+ * Adapter which is able to read sequentially from more CStreams.
+ *
+ * It stores a container of streams and when the actual stream does not
+ * contain more objects, tries to read the next one if any.
+ *
+ * This class is an example of Adapter design pattern. We have to be able to
+ * read from more streams sequentially and this class provides us with the
+ * interface.
+ */
+template<typename Container>
+class CStreamsXpdfReader
+{
+public:
+	typedef std::vector<boost::shared_ptr<CStream> > CStreams;
+
+private:
+	CStreams streams;			/**< Array of streams. */
+	xpdf::XpdfObject xarr;		/**< Xpdf array of streams. */
+	xpdf::XpdfObject curobj;	/**< Current object. */
+
+	boost::shared_ptr<Parser> parser; /**< Xpdf parser. */
+	::Lexer* lexer;					  /**< Xpdf lexer. */
+
+	/**\todo debug */
+	size_t objread;	/**< Helper variable for debugging. Number of read objects. */
+	std::ofstream oss;
+	
+
+public:
+
+	/** Constructor. */
+	CStreamsXpdfReader (Container& strs) :  lexer(NULL), objread (0)
+		{ assert (!strs.empty()); std::copy (strs.begin(), strs.end(), std::back_inserter(streams)); }
+	CStreamsXpdfReader (boost::shared_ptr<CStream> str) : lexer(NULL), objread (0)
+		{ assert (str); streams.push_back (str); }
+
+	/** Open. */
+	void open ()
+	{		
+		assert (!streams.empty());
+		curobj.reset ();
+		
+		// Get xref
+		::XRef* xref = utils::getXRef (streams.front());
+
+		// Create array of streams to parse
+		xarr->initArray (xref);
+		for (CStreams::iterator it = streams.begin(); it != streams.end(); ++it)
+		{
+			assert (hasValidRef(*it) && hasValidPdf (*it));
+			IndiRef rf = (*it)->getIndiRef();
+			xpdf::XpdfObject tmp;
+			tmp->initRef (rf.num, rf.gen);
+			xarr->arrayAdd (tmp.get());
+		}
+		assert (streams.size() == (size_t)xarr->arrayGetLength());
+		
+		// Create parser
+		lexer = new ::Lexer (xref, xarr.get());
+		parser = boost::shared_ptr<Parser> (new ::Parser (xref, lexer));
+
+		/**\todo DEBUG */
+		oss.open ("_stream");
+	}
+
+	/** Close. */
+	void close ()
+	{
+		parser.reset();
+		lexer = NULL;
+		xarr.reset ();
+		curobj.reset ();
+		
+		/**\todo DEBUG */
+		oss.close();
+	}
+
+	/** 
+	 * Close. 
+	 * Save parsed streams to container.
+	 *
+	 * @param parsedstreams Output buffer that will contain all streams we have
+	 * really parsed.
+	 */
+	template<typename Ctr>
+	void close (Ctr& parsedstreams)
+	{
+		assert (!streams.empty());
+		// True only if no exception occurs
+		//assert (actstream->eof());
+		
+		for (size_t i = 0; i < lexer->strIndex(); ++i)
+			parsedstreams.push_back (streams[i]);
+		
+		close();
+		
+		/**\todo DEBUG */
+		oss.close();
+	}
+
+	/** Get xpdf object. */
+	void getXpdfObject (::Object& obj)
+	{
+		parser->getObj (curobj.get());
+		curobj->copy (&obj);
+
+		/** debugging \TODO remove. */
+		objread ++;
+		if (!obj.isEOF() && !obj.isError())
+		{
+			std::string tmp;
+			utils::xpdfObjToString (obj,tmp);
+			oss << objread << " " << tmp << std::endl;
+		}
+	}
+
+	/** 
+	 * Is end of all streams.
+	 *
+	 * We can not cache and due to this fact we can not tell if a stream
+	 * is empty without fetching an object. 
+	 */
+	bool eof ()
+		{ return (curobj->isEOF()); }
+
+	/** Get xpdf stream. */
+	::Stream* getXpdfStream ()
+		{ return parser->getStream(); }
+
+	/**  Are we at the end of actual stream. */
+	bool eofOfActualStream ()
+		{ return (lexer->eof()); }
+
+	
+};
+
+	
+#else // #if CSTREAMSREADER_USE_XPDF
 		
 /**
  * Adapter which is able to read sequentially from more CStreams.
@@ -212,6 +361,8 @@ public:
 
 	
 };
+
+#endif // #if CSTREAMSREADER_USE_XPDF
 
 //=====================================================================================
 } /* namespace pdfobjects */
