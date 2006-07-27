848ca9de7172158aafa9f569b272d37d59ed6dde
Martin Petříček <tux@centrum.cz>
2006-07-27 03:20:25 +0000

    Fix compilability with newer freetype versions

--- src/xpdf/splash/SplashFTFont.cc
+++ src/xpdf/splash/SplashFTFont.cc
@@ -14,7 +14,12 @@
 
 #include <ft2build.h>
 #include FT_OUTLINE_H
+#ifdef FT_INTERNAL_OBJECTS_H
 #include FT_INTERNAL_OBJECTS_H // needed for FT_New_Size decl
+#else
+#include FT_SIZES_H
+#include FT_GLYPH_H
+#endif
 #include "gmem.h"
 #include "SplashMath.h"
 #include "SplashGlyphBitmap.h"
@@ -25,11 +30,11 @@
 
 //------------------------------------------------------------------------
 
-static int glyphPathMoveTo(FT_Vector *pt, void *path);
-static int glyphPathLineTo(FT_Vector *pt, void *path);
-static int glyphPathConicTo(FT_Vector *ctrl, FT_Vector *pt, void *path);
-static int glyphPathCubicTo(FT_Vector *ctrl1, FT_Vector *ctrl2,
-			    FT_Vector *pt, void *path);
+static int glyphPathMoveTo(const FT_Vector *pt, void *path);
+static int glyphPathLineTo(const FT_Vector *pt, void *path);
+static int glyphPathConicTo(const FT_Vector *ctrl, const FT_Vector *pt, void *path);
+static int glyphPathCubicTo(const FT_Vector *ctrl1, const FT_Vector *ctrl2,
+			    const FT_Vector *pt, void *path);
 
 //------------------------------------------------------------------------
 // SplashFTFont
@@ -211,10 +216,10 @@ struct SplashFTFontPath {
 
 SplashPath *SplashFTFont::getGlyphPath(int c) {
   static FT_Outline_Funcs outlineFuncs = {
-    &glyphPathMoveTo,
-    &glyphPathLineTo,
-    &glyphPathConicTo,
-    &glyphPathCubicTo,
+    (FT_Outline_MoveToFunc)(&glyphPathMoveTo),
+    (FT_Outline_LineToFunc)(&glyphPathLineTo),
+    (FT_Outline_ConicToFunc)(&glyphPathConicTo),
+    (FT_Outline_CubicToFunc)(&glyphPathCubicTo),
     0, 0
   };
   SplashFTFontFile *ff;
@@ -249,7 +254,7 @@ SplashPath *SplashFTFont::getGlyphPath(int c) {
   return path.path;
 }
 
-static int glyphPathMoveTo(FT_Vector *pt, void *path) {
+static int glyphPathMoveTo(const FT_Vector *pt, void *path) {
   SplashFTFontPath *p = (SplashFTFontPath *)path;
 
   if (p->needClose) {
@@ -260,7 +265,7 @@ static int glyphPathMoveTo(FT_Vector *pt, void *path) {
   return 0;
 }
 
-static int glyphPathLineTo(FT_Vector *pt, void *path) {
+static int glyphPathLineTo(const FT_Vector *pt, void *path) {
   SplashFTFontPath *p = (SplashFTFontPath *)path;
 
   p->path->lineTo(pt->x / 64.0, -pt->y / 64.0);
@@ -268,7 +273,7 @@ static int glyphPathLineTo(FT_Vector *pt, void *path) {
   return 0;
 }
 
-static int glyphPathConicTo(FT_Vector *ctrl, FT_Vector *pt, void *path) {
+static int glyphPathConicTo(const FT_Vector *ctrl, const FT_Vector *pt, void *path) {
   SplashFTFontPath *p = (SplashFTFontPath *)path;
   SplashCoord x0, y0, x1, y1, x2, y2, x3, y3, xc, yc;
 
@@ -306,8 +311,8 @@ static int glyphPathConicTo(FT_Vector *ctrl, FT_Vector *pt, void *path) {
   return 0;
 }
 
-static int glyphPathCubicTo(FT_Vector *ctrl1, FT_Vector *ctrl2,
-			    FT_Vector *pt, void *path) {
+static int glyphPathCubicTo(const FT_Vector *ctrl1, const FT_Vector *ctrl2,
+			    const FT_Vector *pt, void *path) {
   SplashFTFontPath *p = (SplashFTFontPath *)path;
 
   p->path->curveTo(ctrl1->x / 64.0, -ctrl1->y / 64.0,
