708c38f724c97f96dd3af1cf530bb7d9475875be
Martin Petříček <tux@centrum.cz>
2007-04-14 21:32:26 +0000

    version.xml now generated from version.h bumped version to 0.3.0, release should follow shortly after testing

--- src/gui/version.h
+++ src/gui/version.h
@@ -18,7 +18,7 @@
 /** Application name shown in various About boxes, window titles, etc .... */
 #define APP_NAME "PDFedit"
 /** Version of application. */
-#define VERSION "0.2.5"
+#define VERSION "0.3.0"
 /** Compile time of this program */
 #define COMPILE_TIME (__DATE__ ", " __TIME__)
 
