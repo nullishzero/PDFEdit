5487f00ec6cebef1133d536c1be7066281c2fc52
Martin Petříček <tux@centrum.cz>
2007-06-27 10:36:35 +0000

    Added license header to the perl script

--- src/gui/trans-qs.pl
+++ src/gui/trans-qs.pl
@@ -1,4 +1,13 @@
 #!/usr/bin/perl
+#
+# PDFedit - free program for PDF document manipulation.
+# Copyright (C) 2006, 2007  PDFedit team:      Michal Hocko,
+#                                              Miroslav Jahoda,
+#                                              Jozef Misutka,
+#                                              Martin Petricek
+#
+# Project is hosted on http://sourceforge.net/projects/pdfedit
+#
 
 while (<>) {
  while (s/tr\("([^"]+)"\)//) {
