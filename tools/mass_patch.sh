53ce536bac0170d707eb238f8113b2b5ad47a7de
Martin Petříček <tux@centrum.cz>
2008-02-21 15:38:20 +0000

    added qs to list of files to mass-patch, removed useless cat

--- tools/mass_patch.sh
+++ tools/mass_patch.sh
@@ -51,10 +51,10 @@ echo patch to be used: $PATCH_FILE
 echo directories to be checked: $DIR
 echo dry-run $DRY
 
-for FILE in `find $DIR -type f -iname "*\.cc" -o -iname "*\.h" -o -iname "*\.c"`
+for FILE in `find $DIR -type f -iname "*\.cc" -o -iname "*\.h" -o -iname "*\.c" -o -iname "*\.qs"`
 do
 	echo -n "Patching $FILE "
 #	cat $PATCH_FILE | sed -e "s/\(^---\)\s\(.*\)\s.*/\1 ${FILE}/" -e "s/\(^+++\)\s\(.*\)\s.*/\1 ${FILE}/" | patch -p0 --dry-run && echo ok || echo failed
 
-	cat $PATCH_FILE | patch -p0 $DRY $FILE >& /dev/null && echo -e "\tok" || echo -e "\tfailed"
+	patch <$PATCH_FILE -p0 $DRY $FILE >& /dev/null && echo -e "\tok" || echo -e "\tfailed"
 done
