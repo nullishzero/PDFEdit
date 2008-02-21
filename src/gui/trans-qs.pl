a858343a200f777a9e122a8173edb9b3b13e873e
Michal Hocko <mstsxfx@gmail.com>
2008-02-21 17:02:53 +0000

    license header fixed - Martin has noticed that I have added GPL v2 and *later versions* We haven't discussed this, so I have removed "and later versions" part

--- src/gui/trans-qs.pl
+++ src/gui/trans-qs.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-#
 # PDFedit - free program for PDF document manipulation.
 # Copyright (C) 2006, 2007, 2008  PDFedit team: Michal Hocko,
 #                                              Miroslav Jahoda,
@@ -8,8 +7,7 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation; either version 2 of the License.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +15,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program (in doc/LICENSE.GPL); if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# along with this program (in doc/LICENSE.GPL); if not, write to the 
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
 # MA  02111-1307  USA
 #
 # Project is hosted on http://sourceforge.net/projects/pdfedit
