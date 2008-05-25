53d20808ee31845905f38bd4b648eb33c2861b9a
Michal Hocko <mstsxfx@gmail.com>
2008-05-25 09:07:29 +0000

    version string needs to be distinguished from the upstream released version

--- src/gui/version.h
+++ src/gui/version.h
@@ -32,7 +32,7 @@
 /** Application name shown in various About boxes, window titles, etc .... */
 #define APP_NAME "PDFedit"
 /** Version of application. */
-#define VERSION "0.4.1"
+#define VERSION "0.4.1-CVS"
 /** Compile time of this program */
 #define COMPILE_TIME (__DATE__ ", " __TIME__)
 
