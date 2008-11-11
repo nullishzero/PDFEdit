6c183737b61b5360e24419f5b0ef25c49ab72e4a
Jozef Mišutka <misutkajunior@hotmail.com>
2008-11-11 10:50:00 +0000

    -- finally managed to run kernel tests normally in debug mode with zlib so describing it ---- the interesting part is that it MUST be linked dynamically not statically

--- projects/other/test_zlib/test_zlib.cpp
+++ projects/other/test_zlib/test_zlib.cpp
@@ -1,3 +1,10 @@
+// How to add zlib
+//- download zlib - http://www.zlib.net/
+//- compile DLL, in debug mode it should produce zlib1d.dll & zlib1d.lib
+//- put the zlib1d.lib to projects\output\libs\
+//- add zlib1d.lib to Linker->Input in project Properties 
+//- put the dll to known directory (e.g. from which it is executed)
+
 int main(int argc, char* argv[])
 {
 	return 0;
