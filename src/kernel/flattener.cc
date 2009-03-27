bbf29ed7ac5e02cf84981a45c250ecd04b17bed0
Michal Hocko <mstsxfx@gmail.com>
2009-03-27 14:18:54 +0000

    Introduce Flattener class
    This patch implements bt#289 feature request.
    
    In short:
    
    There are some situations where we want to prevent from incremental updates
    to the original document (e.g. we don't want anybody to see the original
    document).
    
    Our current saving implementation doesn't enable this functionality because
    we are always doing either incremental update saving or up-to-revision
    saving. Both of them preserve the file changes history.
    
    Flattener, on the other hand collects all reachable objects and writes them to
    a new file producing a new document. This means that:
    	- no revision history is kept
    	- all objects which are not reachable from the Trailer are thrown
    	  away
    	- the result document has to same visual output as the original.
    
    This can be used to reduce file size after some operations (e.g. page
    deleting), hide changes done during redaction etc...
    
    Flattener, as well as Delinearizator, is based on PdfDocumentWriter and
    basically implements only fillObjectList method which provides objects
    pre-filled by initReachableObjects which recursively traverses all indirect
    objects reachable from the Trailer.

---
+++ src/kernel/flattener.cc
@@ -0,0 +1,232 @@
+/*
+ * PDFedit - free program for PDF document manipulation.
+ * Copyright (C) 2006, 2007, 2008  PDFedit team: Michal Hocko,
+ *                                              Miroslav Jahoda,
+ *                                              Jozef Misutka,
+ *                                              Martin Petricek
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in doc/LICENSE.GPL); if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ * MA  02111-1307  USA
+ *
+ * Project is hosted on http://sourceforge.net/projects/pdfedit
+ */
+// vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
+#include "kernel/static.h" // WIN32 port - precompiled headers - REMOVE IN FUTURE!
+#include <errno.h>
+#include "kernel/flattener.h"
+#include "utils/debug.h"
+#include "kernel/streamwriter.h"
+#include "kernel/factories.h"
+#include "kernel/pdfedit-core-dev.h"
+
+using namespace pdfobjects;
+using namespace utils;
+
+Flattener::Flattener(FileStreamData &streamData, IPdfWriter * writer)
+	:PdfDocumentWriter(streamData, writer)
+{
+}
+
+boost::shared_ptr<Flattener> Flattener::getInstance(const char * fileName, IPdfWriter * pdfWriter)
+{
+	// creates instance
+	Flattener * instance;
+	boost::shared_ptr<FileStreamData> streamData;
+	try
+	{
+		streamData = boost::shared_ptr<FileStreamData>(PdfDocumentWriter::getStreamData(fileName));
+		if (!streamData)
+			return boost::shared_ptr<Flattener>();
+		instance=new Flattener(*streamData, pdfWriter);
+	}catch(std::exception & e)
+	{
+		// exception thrown from CXref so we have to do a cleanup
+		utilsPrintDbg(debug::DBG_ERR, "Unable to create Flattener instance. Error message="<<e.what());
+		if (streamData->file)
+			fclose(streamData->file);
+		if (streamData->stream)
+			delete streamData->stream;
+		throw e;
+	}
+
+	return boost::shared_ptr<Flattener>(instance, 
+			FileStreamDataDeleter<Flattener>(*streamData));
+}
+
+namespace {
+
+/** Equality operator for refList lookup.
+ */
+bool operator==(const ::Ref& r1, const ::Ref& r2)
+{
+	return r1.num == r2.num && r1.gen== r2.gen;
+}
+
+bool containsRef(const Flattener::RefList & refList, const ::Ref& ref)
+{
+	Flattener::RefList::const_iterator i;
+	for(i=refList.begin(); i!=refList.end(); ++i)
+		if(*i == ref)
+			return true;
+	return false;
+}
+
+// fwd declaration
+void collectReacableRefs(::XRef& xref, ::Object &obj, Flattener::RefList &refList);
+
+/** Helper function to find all references from given dictionary.
+ * @param xref XRef table.
+ * @param dict Dictionary to be examined.
+ * @param refList List of already collected references.
+ * 
+ */
+void collectDictRefElems(::XRef &xref, ::Dict &dict, Flattener::RefList &refList)
+{
+	for(int i=0; i<dict.getLength(); i++)
+	{
+		Object elem;
+		if(!dict.getValNF(i, &elem))
+		{
+			utilsPrintDbg(debug::DBG_ERR, "Unable to get dictionary entry with index "<<i);
+			throw MalformedFormatExeption("bad data stream");
+		}
+		collectReacableRefs(xref, elem, refList);
+		elem.free();
+	}
+}
+
+/** Collects all reachable objects from the given one.
+ * @param xref XRef table.
+ * @param obj Object to traverse.
+ * @param refList List of alreadt collected references.
+ *
+ * Fills the given list with references which are recursively reachable 
+ * from the given object.
+ * <br>
+ * If you start with the Trailer then you will collect all reachable 
+ * objects.
+ */
+void collectReacableRefs(XRef& xref, ::Object &obj, Flattener::RefList &refList)
+{
+	switch(obj.getType())
+	{
+		case objArray:
+			for(int i=0; i<obj.arrayGetLength(); i++)
+			{
+				Object elem;
+				if(!obj.arrayGetNF(i, &elem))
+				{
+					utilsPrintDbg(debug::DBG_ERR, "Unable to get array entry");
+					throw MalformedFormatExeption("bad data stream");
+				}
+				collectReacableRefs(xref, elem, refList);
+				elem.free();
+			}
+			break;
+		case objDict:
+		{
+			Dict *dict = obj.getDict();
+			collectDictRefElems(xref, *dict, refList);
+			break;
+		}
+		case objStream:
+		{
+			Dict *streamDict = obj.streamGetDict();
+			collectDictRefElems(xref, *streamDict, refList);
+			break;
+		}
+		case objRef:
+		{
+			::Ref ref = obj.getRef();
+			// check for already seen referencies and skip them
+			if (containsRef(refList, ref))
+				return;
+			// TODO should be sorted by offset to keep the same
+			// ordering in the file as the original document
+			refList.push_back(ref);
+			Object target;
+			if(!obj.fetch(&xref, &target) || !xref.isOk())
+			{
+				kernelPrintDbg(debug::DBG_ERR, ref<<" object fetching failed with code="
+						<<xref.getErrorCode());
+				obj.free();
+				throw MalformedFormatExeption("bad data stream");
+			}
+			collectReacableRefs(xref, target, refList);
+			target.free();
+			break;
+		}
+		default:
+			// nothing really interesting here
+			break;
+	}
+}
+} // annonymous namespace
+
+void Flattener::initReachableObjects()
+{
+	utilsPrintDbg(debug::DBG_DBG, "Creating a list of the reachable objects");
+	reachAbleRefs.clear();
+	
+	// traverses all objects reachable from trailer and put their references
+	// to the reachAbleRefs - this should provide complete list of all objects
+	// required for document
+	Object *trailer = getTrailerDict();
+	collectReacableRefs(*this, *trailer, reachAbleRefs);
+	utilsPrintDbg(debug::DBG_INFO, reachAbleRefs.size()<<" indirect objects collected");
+	lastIndex=0;
+}
+
+int Flattener::flatten(const char * fileName)
+{
+	initReachableObjects();
+	return writeDocument(fileName);
+}
+
+int Flattener::flatten(FILE * file)
+{
+	initReachableObjects();
+	return writeDocument(file);
+}
+
+int Flattener::fillObjectList(IPdfWriter::ObjectList &objectList, int maxObjectCount)
+{
+	// collects (fetches) all objects from XRef::entries array, which are not 
+	// free, to the objectList. Skips also Linearized dictionary
+	utilsPrintDbg(debug::DBG_DBG, "Collecting objects starting from "<<lastIndex);
+	objectList.clear();
+	for(; lastIndex < reachAbleRefs.size(); lastIndex++)
+	{
+		::Ref ref = reachAbleRefs[lastIndex];
+		// gets object and generation number
+		int num=ref.num, gen=ref.gen;
+
+		// stop if we reach the maximum objects
+		if(maxObjectCount>0 && objectList.size()>=(size_t)maxObjectCount)
+			break;
+
+		::Object * obj=XPdfObjectFactory::getInstance();
+		XRef::fetch(num, gen, obj);
+		if(!isOk())
+		{
+			kernelPrintDbg(debug::DBG_ERR, ref<<" object fetching failed with code="
+					<<errCode);
+			xpdf::freeXpdfObject(obj);
+			throw MalformedFormatExeption("bad data stream");
+		}
+		objectList.push_back(IPdfWriter::ObjectElement(ref, obj));
+	}
+	utilsPrintDbg(debug::DBG_DBG, "Returned "<<objectList.size()<<" objects");
+	return objectList.size();
+}
---
+++ src/kernel/flattener.h
@@ -0,0 +1,145 @@
+/*
+ * PDFedit - free program for PDF document manipulation.
+ * Copyright (C) 2006, 2007, 2008  PDFedit team: Michal Hocko,
+ *                                              Miroslav Jahoda,
+ *                                              Jozef Misutka,
+ *                                              Martin Petricek
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in doc/LICENSE.GPL); if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ * MA  02111-1307  USA
+ *
+ * Project is hosted on http://sourceforge.net/projects/pdfedit
+ */
+// vim:tabstop=4:shiftwidth=4:noexpandtab:textwidth=80
+#ifndef _FLATTENER_H_
+#define _FLATTENER_H_
+
+#include "kernel/xpdf.h"
+#include "kernel/exceptions.h"
+#include "kernel/pdfwriter.h"
+
+namespace pdfobjects 
+{
+namespace utils
+{
+
+/** Flattener class.
+ * Provides functionality to write the new PDF document with the 
+ * all reachable objects from the original document in the single 
+ * revision.
+ * <br>
+ * This can be usefull for several reasons:
+ * <ul>
+ * <li>To get rid of older revisions
+ * <li>To get rid of objects which cannot affect document displaying (because
+ * they are not reachable) and so make the result file smaller.
+ * <li>To hide changes made during editing (e.g. they are confidential)
+ * </ul>
+ * <p>
+ * <b>Usage</b>
+ * Use static factory method for instance creation:
+ * <pre>
+ * // we will use OldStylePdfWriter IPdfWriter implementator
+ * IPdfWriter * contentWriter=new OldStylePdfWriter();
+ * boost::shared_ptr<Flattener> flattener = Flattener::getInstance(fileName, contentWriter);
+ *
+ * // check for encryption and set credentials if necessary
+ * if (flattener->isEncrypted())
+ * 	flattener->setCredentials(ownerPasswd, userPasswd);
+ *
+ * // flatten file content to the file specified by name
+ * flattener->delinearize(outputFile);
+ *
+ * ...
+ *
+ * // instance is wrapped by the smart pointer so you don't
+ * // have bother with deallocation
+ * </pre>
+ */
+class Flattener: public PdfDocumentWriter
+{
+public:
+	// TODO maybe some more lookup efficient structure
+	typedef std::vector<Ref> RefList;
+
+	/** List of all reachable indirect objects.
+	 * Initialized in initReachableObjects.
+	 */
+	RefList reachAbleRefs;
+
+private:
+	/** Index of the last in the reachAbleRefs returned object by fillObjectList.
+	 * Zeroed in flatten methods.
+	 */
+	size_t lastIndex;
+
+	virtual ~Flattener() {};
+
+	// deallocator for this class
+	friend class FileStreamDataDeleter<Flattener>;
+
+protected:
+	Flattener(FileStreamData &streamData, IPdfWriter * writer);
+
+	/** Initializes all reachable objects.
+	 *
+	 * Starts with the Trailer and recursively travels all reachable
+	 * indirect objects which are stored in reachAbleRefs container.
+	 */
+	void initReachableObjects();
+
+	/** Fills up to given maxObjectCount into the given list.
+	 * @param objectList Container for objects.
+	 * @param maxObjectCount Maximum objects count to be filled.
+	 *
+	 * Note that given list is cleared at the beggining.
+	 * @return number of objects filled into the container.
+	 */
+	virtual int fillObjectList(IPdfWriter::ObjectList &objectList, int maxObjectCount);
+public:
+	/** Factory method.
+	 * @param fileName Input PDF document.
+	 * @param pdfWriter PDF Writer to be used for content writing.
+	 * @throw MalformedFormatExeption if file content is not valid pdf document.
+	 * @return Instance ready to be used.
+	 */
+	static boost::shared_ptr<Flattener> getInstance(const char * fileName, IPdfWriter * pdfWriter);
+
+	/** Flattens this document and puts the result into the given file.
+	 * @param fileName Output file name.
+	 *
+	 * Delegates to PdfDocumentWriter::writeDocument(const char*).
+	 *
+	 * @return 0 on success, errno otherwise.
+	 * @throw NotImplementedException if document is encrypted.
+	 * @throw MalformedFormatExeption if the input file is currupted.
+	 */
+	int flatten(const char *fileName);
+
+	/** Flattens this document and puts the result into the given file.
+	 * @param file File handle where to put data.
+	 *
+	 * Delegates to PdfDocumentWriter::writeDocument(FILE*).
+	 * @throw NotImplementedException if document is encrypted.
+	 * @throw MalformedFormatExeption if the input file is currupted.
+	 */
+	int flatten(FILE * file);
+
+};
+
+} // namespace utils
+} // namespace pdfobjects
+
+#endif
+
