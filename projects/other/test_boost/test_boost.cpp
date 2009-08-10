f417c1a3264fedbc36d746e72abf6986bcf5512b
Jozef Mišutka <misutkajunior@hotmail.com>
2009-08-10 14:06:54 +0000

    added better zlib/boost checks

--- projects/other/test_boost/test_boost.cpp
+++ projects/other/test_boost/test_boost.cpp
@@ -1,6 +1,14 @@
+// How to add boost
+//- download boost - http://www.boost.org/ or http://www.boostpro.com/download
+//- add include/libs to VS Tools->Options->Directories
+
+#include <boost/shared_ptr.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string/replace.hpp>
 
 int main(int , char* [])
 {
+	boost::shared_ptr<int> i;
 	return 0;
 }
 
--- projects/other/test_zlib/test_zlib.cpp
+++ projects/other/test_zlib/test_zlib.cpp
@@ -5,8 +5,11 @@
 //- add zlib1d.lib to Linker->Input in project Properties 
 //- put the dll to known directory (e.g. from which it is executed)
 
+#include <zlib.h>
+
 int main(int , char* [])
 {
+	z_stream z;
 	return 0;
 }
 
